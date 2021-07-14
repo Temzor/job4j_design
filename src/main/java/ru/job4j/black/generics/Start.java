9f282a9f7f95fdb44d0f74e8cd341b8d7b8c152b
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-14 11:01:14 +0300

    error correction

--- src/main/java/ru/job4j/black/generics/Start.java
+++ src/main/java/ru/job4j/black/generics/Start.java
@@ -1,18 +1,17 @@
 package ru.job4j.black.generics;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Start {
     public static void main(String[] args) {
-        List<String> list = new ArrayList();
+        ArrayList<String> list = new ArrayList<>();
         list.add("Hi");
         list.add("By");
         list.add("OK");
         list.add("Good");
 
-        for (Object o : list) {
-            System.out.println(o + " length " + ((String) o).length());
+        for (String o : list) {
+            System.out.println(o + " length " + o.length());
         }
 
     }
