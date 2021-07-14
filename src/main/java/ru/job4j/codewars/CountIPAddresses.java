7abd36d82dd8dee332a69b4dcb00f14079dfa5c4
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-14 10:58:45 +0300

    Valid Parentheses

--- src/main/java/ru/job4j/codewars/CountIPAddresses.java
+++ src/main/java/ru/job4j/codewars/CountIPAddresses.java
@@ -8,8 +8,8 @@ public class CountIPAddresses {
     public static long ipsBetween(String start, String end) {
         long result = 0;
 
-        String [] ad1 = start.split("\\.");
-        String [] ad2 = end.split("\\.");
+        String[] ad1 = start.split("\\.");
+        String[] ad2 = end.split("\\.");
         result += (Long.parseLong(ad2[0]) - Long.parseLong(ad1[0])) * 256 * 256 * 256;
         result += (Long.parseLong(ad2[1]) - Long.parseLong(ad1[1])) * 256 * 256;
         result += (Long.parseLong(ad2[2]) - Long.parseLong(ad1[2])) * 256;
