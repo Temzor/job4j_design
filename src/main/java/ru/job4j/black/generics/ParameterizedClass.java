36dbb5b1e8dd998fd07f5e149971945416b24ac9
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-15 14:41:39 +0300

    3. Generics. 2. Parameterized class.

---
+++ src/main/java/ru/job4j/black/generics/ParameterizedClass.java
@@ -0,0 +1,43 @@
+package ru.job4j.black.generics;
+
+public class ParameterizedClass {
+    public static void main(String[] args) {
+
+        Info<String> stringInfo = new Info<>("Privet");
+        System.out.println(stringInfo);
+        String s = stringInfo.getValue();
+
+        Info<Integer> integerInfo = new Info<>(1);
+        System.out.println(integerInfo);
+        Integer i = integerInfo.getValue();
+
+
+        Info<Boolean> booleanInfo = new Info<>(false);
+        System.out.println(booleanInfo);
+        Boolean b = booleanInfo.getValue();
+
+        System.out.println(s + " " + i + " " + b);
+
+
+    }
+
+}
+
+class Info<T> {
+    private T value;
+
+    public Info(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Info{"
+                + "value=" + value
+                + '}';
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
