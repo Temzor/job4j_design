1d54870af88bb221cfafd158f325625b48a27ef5
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-18 12:18:30 +0300

    Reversed Strings

---
+++ src/main/java/ru/job4j/codewars/ReversedStrings.java
@@ -0,0 +1,8 @@
+package ru.job4j.codewars;
+
+public class ReversedStrings {
+    public static String solution(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+}
---
+++ src/test/java/ru/job4j/codewars/ReversedStringsTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReversedStringsTest {
+    @Test
+    public void sampleTests() {
+        assertEquals("dlrow", ReversedStrings.solution("world"));
+    }
+}
