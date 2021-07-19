c5794ea3e16036b26f828b31c8364181202d864d
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-19 14:35:23 +0300

    Abbreviate a Two Word Name

---
+++ src/main/java/ru/job4j/codewars/AbbreviateTwoWords.java
@@ -0,0 +1,9 @@
+package ru.job4j.codewars;
+
+
+public class AbbreviateTwoWords {
+    public static String abbrevName(String name) {
+        String[] separator = name.split(" ");
+        return (separator[0].charAt(0) + "." + separator[1].charAt(0)).toUpperCase();
+    }
+}
