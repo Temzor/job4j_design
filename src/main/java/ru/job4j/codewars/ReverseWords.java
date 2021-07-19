92fe7a9ea33c90377d672ee214b2e1fb75ecdeb4
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-19 22:35:30 +0300

    Reversed Words

---
+++ src/main/java/ru/job4j/codewars/ReverseWords.java
@@ -0,0 +1,11 @@
+package ru.job4j.codewars;
+
+import java.util.Arrays;
+
+public class ReverseWords {
+    public static String reverseWords(String str) {
+        return Arrays.stream(str.split(" "))
+                .reduce((k, v) -> v + " " + k)
+                .get();
+    }
+}
---
+++ src/test/java/ru/job4j/codewars/ReverseWordsTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReverseWordsTest {
+    @Test
+    public void testSomething() {
+        assertEquals("eating like I", ReverseWords.reverseWords("I like eating"));
+        assertEquals("flying like I", ReverseWords.reverseWords("I like flying"));
+        assertEquals("nice is world The", ReverseWords.reverseWords("The world is nice"));
+    }
+
+}
