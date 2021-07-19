6f27b58303a2c9e6a21f413bf4f98c8bce0ada3d
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-19 22:29:43 +0300

    Sum without highest and lowest number

---
+++ src/main/java/ru/job4j/codewars/SumWithoutHighest.java
@@ -0,0 +1,16 @@
+package ru.job4j.codewars;
+
+import java.util.Arrays;
+
+public class SumWithoutHighest {
+    public static int sum(int[] numbers) {
+        if (numbers == null || numbers.length < 2) {
+            return 0;
+        }
+        return Arrays.stream(numbers)
+                .sorted()
+                .skip(1)
+                .limit(numbers.length - 2)
+                .sum();
+    }
+}
