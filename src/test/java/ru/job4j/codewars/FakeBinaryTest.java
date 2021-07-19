a8a9bc151ad83daac527bdcdc9b5440cc690c946
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-19 22:16:29 +0300

    Fake Binary

---
+++ src/test/java/ru/job4j/codewars/FakeBinaryTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FakeBinaryTest {
+    @Test
+    public void testSomething() {
+        assertEquals("01011110001100111", FakeBinary.fakeBin("45385593107843568"));
+        assertEquals("101000111101101", FakeBinary.fakeBin("509321967506747"));
+        assertEquals("011011110000101010000011011",
+                FakeBinary.fakeBin("366058562030849490134388085"));
+        assertEquals("", FakeBinary.fakeBin(""));
+    }
+
+}
