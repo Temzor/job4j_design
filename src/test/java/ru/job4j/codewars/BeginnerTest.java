c20ec13bb52e80989d9c6d78020f4c377030f9b8
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-20 09:52:36 +0300

    You only need one - Beginner

---
+++ src/test/java/ru/job4j/codewars/BeginnerTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BeginnerTest {
+    @Test
+    public void testSomething() {
+        assertTrue(Beginner.check(new Object[]{66, 101}, 66));
+        assertTrue(Beginner.check(new Object[]{80, 117, 115, 104, 45, 85, 112, 115}, 45));
+        assertTrue(Beginner.check(new Object[]{'t', 'e', 's', 't'}, 'e'));
+        assertFalse(Beginner.check(new Object[]{"what", "a", "great", "kata"}, "kat"));
+    }
+
+}
