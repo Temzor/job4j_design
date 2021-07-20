1ee2037321ed812980a5365767c43a3032d18382
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-20 10:16:54 +0300

    Jenny's secret message

---
+++ src/test/java/ru/job4j/codewars/GreeterTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GreeterTest {
+    @Test
+    public void test() {
+        assertEquals("should greet some people normally",
+                "Hello, Jim!", Greeter.greet("Jim"));
+        assertEquals("should greet some people normally",
+                "Hello, Jane!", Greeter.greet("Jane"));
+        assertEquals("should greet some people normally",
+                "Hello, Simon!", Greeter.greet("Simon"));
+        assertEquals("should greet Johnny a little bit more special",
+                "Hello, my love!", Greeter.greet("Johnny"));
+    }
+
+}
