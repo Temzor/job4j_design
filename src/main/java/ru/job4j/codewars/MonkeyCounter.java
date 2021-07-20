668131bdba1a1f8c02a4f9d3739cdbf4db56212d
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-20 09:45:44 +0300

    Count the Monkeys!

---
+++ src/main/java/ru/job4j/codewars/MonkeyCounter.java
@@ -0,0 +1,9 @@
+package ru.job4j.codewars;
+
+import java.util.stream.IntStream;
+
+public class MonkeyCounter {
+    public static int[] monkeyCount(final int n) {
+        return IntStream.rangeClosed(1, n).toArray();
+    }
+}
