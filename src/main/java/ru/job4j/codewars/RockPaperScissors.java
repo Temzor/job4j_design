e9301fc9471bdf2cf69ddb641292381d8255910d
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-20 11:29:33 +0300

    Rock Paper Scissors!

---
+++ src/main/java/ru/job4j/codewars/RockPaperScissors.java
@@ -0,0 +1,25 @@
+package ru.job4j.codewars;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RockPaperScissors {
+    public static String rps(String p1, String p2) {
+        if (p1.equals(p2)) {
+            return "Draw!";
+        }
+
+        Map<String, String> game = new HashMap<>();
+        game.put("scissors", "paper");
+        game.put("paper", "rock");
+        game.put("rock", "scissors");
+
+        String result = game.get(p1);
+        if (result.equals(p2)) {
+            return "Player 1 won!";
+        } else {
+            return "Player 2 won!";
+        }
+    }
+}
+
