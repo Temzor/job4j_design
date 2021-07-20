9f51359f6483b036c2ce516c48f7cbb5596acde6
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-20 19:11:46 +0300

    Beginner Series #4 Cockroach

---
+++ src/main/java/ru/job4j/codewars/Cockroach.java
@@ -0,0 +1,7 @@
+package ru.job4j.codewars;
+
+public class Cockroach {
+    public int cockroachSpeed(double x) {
+        return (int) Math.floor(x / 0.036);
+    }
+}
---
+++ src/test/java/ru/job4j/codewars/CockroachTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CockroachTest {
+    @Test
+    public void basicTests() {
+        Cockroach cockroach = new Cockroach();
+        assertEquals(30, cockroach.cockroachSpeed(1.08));
+        assertEquals(30, cockroach.cockroachSpeed(1.09));
+        assertEquals(0, cockroach.cockroachSpeed(0));
+    }
+
+}
