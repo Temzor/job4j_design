46cc94f4b0c0af462fa2a7d9c784f21e1db89a05
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-20 18:20:13 +0300

    Are You Playing Banjo?

---
+++ src/test/java/ru/job4j/codewars/BanjoTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BanjoTest {
+    @Test
+    public void peopleThatPlayBanjo() {
+        assertEquals("Nope!", "Martin does not play banjo", Banjo.areYouPlayingBanjo("Martin"));
+        assertEquals("Nope!", "Rikke plays banjo", Banjo.areYouPlayingBanjo("Rikke"));
+    }
+
+}
