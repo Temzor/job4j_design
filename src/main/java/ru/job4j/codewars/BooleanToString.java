7728d5d4d65354f00face765cb894191dffc5729
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-20 11:53:04 +0300

    Convert a Boolean to a String

--- src/main/java/ru/job4j/codewars/BooleanToString.java
+++ src/main/java/ru/job4j/codewars/BooleanToString.java
@@ -2,11 +2,6 @@ package ru.job4j.codewars;
 
 public class BooleanToString {
     public static String convert(boolean b) {
-        if (b) {
-            return "true";
-        } else {
-            return "false";
-        }
+        return b ? "true" : "false";
     }
-
 }
