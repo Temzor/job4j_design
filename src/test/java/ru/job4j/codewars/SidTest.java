73f3320d5058b7a24370f440d9804020de7e58dc
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-20 18:48:19 +0300

    Simple multiplication

---
+++ src/test/java/ru/job4j/codewars/SidTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SidTest {
+    @Test
+    public void test1() {
+        System.out.println("Checking for Odd numbers.");
+        assertEquals(9, Sid.simpleMultiplication(1));
+        assertEquals(27, Sid.simpleMultiplication(3));
+        assertEquals(189, Sid.simpleMultiplication(21));
+        assertEquals(207, Sid.simpleMultiplication(23));
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Checking for Even numbers.");
+        assertEquals(16, Sid.simpleMultiplication(2));
+        assertEquals(32, Sid.simpleMultiplication(4));
+        assertEquals(176, Sid.simpleMultiplication(22));
+        assertEquals(208, Sid.simpleMultiplication(26));
+    }
+
+}
