a67f9630eee9bebc546578bf56d2584d74da4697
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-21 11:15:25 +0300

    Will there be enough space?

---
+++ src/main/java/ru/job4j/codewars/Bob.java
@@ -0,0 +1,7 @@
+package ru.job4j.codewars;
+
+public class Bob {
+    public static int enough(int cap, int on, int wait) {
+        return cap - on - wait  < wait ? Math.abs(cap - on - wait) : 0;
+    }
+}
