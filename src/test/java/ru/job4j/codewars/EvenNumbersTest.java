a971834c120116599d57585ca211500f72f743b7
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-21 11:54:49 +0300

    Find numbers which are divisible by given number

---
+++ src/test/java/ru/job4j/codewars/EvenNumbersTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EvenNumbersTest {
+    @Test
+    public void testSimple() {
+        assertArrayEquals(new int[] {2, 4, 6}, EvenNumbers.divisibleBy(new int[] {1, 2, 3, 4, 5, 6}, 2));
+        assertArrayEquals(new int[] {3, 6}, EvenNumbers.divisibleBy(new int[] {1, 2, 3, 4, 5, 6}, 3));
+        assertArrayEquals(new int[] {0, 4}, EvenNumbers.divisibleBy(new int[] {0, 1, 2, 3, 4, 5, 6}, 4));
+    }
+
+}
