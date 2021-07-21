6defd3722fcb82c783b111a066a7625fdfd3f3ba
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-21 11:49:24 +0300

    Opposites Attract

---
+++ src/test/java/ru/job4j/codewars/OppositesAttractTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OppositesAttractTest {
+    @Test
+    public void testOddAndEven() {
+        assertTrue(OppositesAttract.isLove(1, 4));
+    }
+
+    @Test
+    public void testEvenAndEven() {
+        assertFalse(OppositesAttract.isLove(2, 2));
+    }
+
+    @Test
+    public void testOddAndOdd() {
+        assertFalse(OppositesAttract.isLove(1, 1));
+    }
+
+    @Test
+    public void testEvenAndOdd() {
+        assertTrue(OppositesAttract.isLove(0, 1));
+    }
+
+}
