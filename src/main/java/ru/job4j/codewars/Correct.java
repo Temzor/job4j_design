5de2bf1bafd9e495db795ba7d064fb5b606553bb
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-21 12:23:17 +0300

    return string.replace("5", "S")                 .replace("0", "O")                 .replace("1", "I");

---
+++ src/main/java/ru/job4j/codewars/Correct.java
@@ -0,0 +1,9 @@
+package ru.job4j.codewars;
+
+public class Correct {
+    public static String correct(String string) {
+        return string.replace("5", "S")
+                .replace("0", "O")
+                .replace("1", "I");
+    }
+}
