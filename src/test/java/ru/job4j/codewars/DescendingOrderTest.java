acab19dd0fac53ec604abc83ee1950651e661995
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-23 16:07:35 +0300

    Descending Order

---
+++ src/test/java/ru/job4j/codewars/DescendingOrderTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DescendingOrderTest {
+    @Test
+    public void test01() {
+        assertEquals(0, DescendingOrder.sortDesc(0));
+    }
+
+    @Test
+    public void test02() {
+        assertEquals(51, DescendingOrder.sortDesc(15));
+    }
+
+
+    @Test
+    public void test03() {
+        assertEquals(987654321, DescendingOrder.sortDesc(123456789));
+    }
+
+}
