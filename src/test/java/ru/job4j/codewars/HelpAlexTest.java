b92b8dc0eeced58ace2072131f4254d7f6705e19
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-24 19:06:45 +0300

    Help Alex

---
+++ src/test/java/ru/job4j/codewars/HelpAlexTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelpAlexTest {
+    @Test
+    public void testHoopCount() {
+        assertEquals("Great, now move on to tricks", HelpAlex.hoopCount(11));
+        assertEquals("Keep at it until you get it", HelpAlex.hoopCount(7));
+    }
+
+}
