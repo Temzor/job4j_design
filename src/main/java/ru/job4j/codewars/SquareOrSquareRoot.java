45cb6220077ced39a2fec1f99103440cf430cfd6
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-24 19:39:05 +0300

    To square(root) or not to square(root)

---
+++ src/main/java/ru/job4j/codewars/SquareOrSquareRoot.java
@@ -0,0 +1,11 @@
+package ru.job4j.codewars;
+
+import java.util.Arrays;
+
+public class SquareOrSquareRoot {
+    public static int[] squareOrSquareRoot(int[] array) {
+        return Arrays.stream(array)
+                .map(i -> Math.sqrt(i) % 1 == 0 ? ((int) Math.sqrt(i)) : (i * i))
+                .toArray();
+    }
+}
---
+++ src/test/java/ru/job4j/codewars/SquareOrSquareRootTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class SquareOrSquareRootTest {
+    @Test
+    public void basicTests() {
+        int[] input = new int[] {4, 3, 9, 7, 2, 1 };
+        int[] expected = new int[] {2, 9, 3, 49, 4, 1 };
+        assertEquals(Arrays.toString(expected), Arrays.toString(SquareOrSquareRoot.squareOrSquareRoot(input)));
+
+        input = new int[] {100, 101, 5, 5, 1, 1 };
+        expected = new int[] {10, 10201, 25, 25, 1, 1 };
+        assertEquals(Arrays.toString(expected), Arrays.toString(SquareOrSquareRoot.squareOrSquareRoot(input)));
+
+        input = new int[] {1, 2, 3, 4, 5, 6 };
+        expected = new int[] {1, 4, 9, 2, 25, 36 };
+        assertEquals(Arrays.toString(expected), Arrays.toString(SquareOrSquareRoot.squareOrSquareRoot(input)));
+    }
+
+
+}
