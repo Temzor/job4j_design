976708e59365d6f7d3bc10f3cda19a105aa73d3f
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-26 10:04:54 +0300

    Unfinished Loop - Bug Fixing #1

--- src/test/java/ru/job4j/codewars/CreateListTest.java
+++ src/test/java/ru/job4j/codewars/CreateListTest.java
@@ -3,19 +3,19 @@ package ru.job4j.codewars;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static org.junit.Assert.*;
 
 public class CreateListTest {
-    CreateList k = new CreateList();
-
     @Test
     public void sampleTests() {
-        assertEquals(Arrays.asList(1), k.createList(1));
-        assertEquals(Arrays.asList(1, 2), k.createList(2));
-        assertEquals(Arrays.asList(1, 2, 3), k.createList(3));
-        assertEquals(Arrays.asList(1, 2, 3, 4), k.createList(4));
-        assertEquals(Arrays.asList(1, 2, 3, 4, 5), k.createList(5));
+        assertEquals(Collections.singletonList(1), CreateList.createList(1));
+        assertEquals(Arrays.asList(1, 2),
+                CreateList.createList(2));
+        assertEquals(Arrays.asList(1, 2, 3), CreateList.createList(3));
+        assertEquals(Arrays.asList(1, 2, 3, 4), CreateList.createList(4));
+        assertEquals(Arrays.asList(1, 2, 3, 4, 5), CreateList.createList(5));
     }
 
 }
