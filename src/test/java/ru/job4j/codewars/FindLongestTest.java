404468c35dcf791ee132b4e50eca68177f85265c
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-26 09:58:39 +0300

    Squash the bugs

---
+++ src/test/java/ru/job4j/codewars/FindLongestTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FindLongestTest {
+
+    @Test
+    public void basicTests() {
+        // assertEquals("expected", "actual");
+        assertEquals(7, FindLongest.findLongest("The quick white fox jumped around the massive dog"));
+        assertEquals(10, FindLongest.findLongest("Take me to tinseltown with you"));
+        assertEquals(7, FindLongest.findLongest("Sausage chops"));
+        assertEquals(6, FindLongest.findLongest("Wind your body and wiggle your belly"));
+        assertEquals(7, FindLongest.findLongest("Lets all go on holiday"));
+    }
+
+}
