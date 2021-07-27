8f790a44d01e4569b609a46b4d3ed254e7a45e2e
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-27 09:13:08 +0300

    Powers of 2

---
+++ src/test/java/ru/job4j/codewars/PowersOfTwoTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PowersOfTwoTest {
+    @Test
+    public void testSomething() {
+        assertArrayEquals(new long[]{1}, PowersOfTwo.powersOfTwo(0));
+        assertArrayEquals(new long[]{1, 2}, PowersOfTwo.powersOfTwo(1));
+        assertArrayEquals(new long[]{1, 2, 4, 8, 16}, PowersOfTwo.powersOfTwo(4));
+    }
+
+}
