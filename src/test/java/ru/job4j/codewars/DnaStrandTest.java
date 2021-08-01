b6628151117d58d63fc841a37cc1654c52c5c446
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-01 18:23:48 +0300

    Complementary DNA

---
+++ src/test/java/ru/job4j/codewars/DnaStrandTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DnaStrandTest {
+    @Test
+    public void test01() {
+        assertEquals("TTTT", DnaStrand.makeComplement("AAAA"));
+    }
+    @Test
+    public void test02() {
+        assertEquals("TAACG", DnaStrand.makeComplement("ATTGC"));
+    }
+    @Test
+    public void test03() {
+        assertEquals("CATA", DnaStrand.makeComplement("GTAT"));
+    }
+
+}
