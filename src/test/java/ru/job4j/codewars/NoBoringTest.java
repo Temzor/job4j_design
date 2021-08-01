8a6180f38c05d8e9f7efd278d09794516a7b2886
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-01 17:17:06 +0300

    No zeros for heros

---
+++ src/test/java/ru/job4j/codewars/NoBoringTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NoBoringTest {
+    private static void testing(int actual, int expected) {
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test1() {
+        System.out.println("Fixed Tests: noBoringZeros");
+        testing(NoBoring.noBoringZeros(1450), 145);
+        testing(NoBoring.noBoringZeros(960000), 96);
+        testing(NoBoring.noBoringZeros(1050), 105);
+        testing(NoBoring.noBoringZeros(-1050), -105);
+    }
+
+}
