20aa8639df1629277b06ac9e0bc280a21f9d0bf8
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-03 17:54:49 +0300

    Sum of odd numbers

---
+++ src/main/java/ru/job4j/codewars/RowSumOddNumbers.java
@@ -0,0 +1,7 @@
+package ru.job4j.codewars;
+
+public class RowSumOddNumbers {
+    public static int rowSumOddNumbers(int n) {
+        return (int) Math.pow(n, 3);
+    }
+}
---
+++ src/test/java/ru/job4j/codewars/RowSumOddNumbersTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RowSumOddNumbersTest {
+    @Test
+    public void test1() {
+        assertEquals(1, RowSumOddNumbers.rowSumOddNumbers(1));
+        assertEquals(74088, RowSumOddNumbers.rowSumOddNumbers(42));
+    }
+
+}
