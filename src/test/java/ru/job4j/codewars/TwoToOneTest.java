d21fade14a8e0d1687d4e86450f108eeb1c94041
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-03 17:48:42 +0300

    Two to One

---
+++ src/test/java/ru/job4j/codewars/TwoToOneTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TwoToOneTest {
+    @Test
+    public void test() {
+        System.out.println("longest Fixed Tests");
+        assertEquals("aehrsty", TwoToOne.longest("aretheyhere", "yestheyarehere"));
+        assertEquals("abcdefghilnoprstu", TwoToOne.longest("loopingisfunbutdangerous", "lessdangerousthancoding"));
+        assertEquals("acefghilmnoprstuy", TwoToOne.longest("inmanylanguages", "theresapairoffunctions"));
+    }
+
+}
