a80ecb495224d1489fa541e725b6a7bfea7a7cd1
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-04 19:24:03 +0300

    String ends with?

---
+++ src/main/java/ru/job4j/codewars/StringEndsWith.java
@@ -0,0 +1,7 @@
+package ru.job4j.codewars;
+
+public class StringEndsWith {
+    public static boolean solution(String str, String ending) {
+        return str.endsWith(ending);
+    }
+}
---
+++ src/test/java/ru/job4j/codewars/StringEndsWithTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringEndsWithTest {
+    @Test
+    public void testSomething() {
+        assertTrue(StringEndsWith.solution("samurai", "ai"));
+        assertTrue(StringEndsWith.solution("abc", "bc"));
+        assertFalse(StringEndsWith.solution("abc", "d"));
+        assertFalse(StringEndsWith.solution("sumo", "omo"));
+    }
+
+}
