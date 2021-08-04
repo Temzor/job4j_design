a4c63c338f1761d7282828230a7c8a4d03d0699d
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-04 18:54:40 +0300

    Odd or Even?

---
+++ src/main/java/ru/job4j/codewars/OddOrEven.java
@@ -0,0 +1,9 @@
+package ru.job4j.codewars;
+
+import java.util.Arrays;
+
+public class OddOrEven {
+    public static String oddOrEven(int[] array) {
+        return Arrays.stream(array).sum() % 2 != 0 ? "odd" : "even";
+    }
+}
