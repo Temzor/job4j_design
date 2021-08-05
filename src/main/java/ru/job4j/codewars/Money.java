2afb7020ae67058937c10e3c898f5bd58406a202
Temzor <onewaykd@gmail.com>
2021-08-05 16:21:19 +0300

    Money, Money, Money

---
+++ src/main/java/ru/job4j/codewars/Money.java
@@ -0,0 +1,12 @@
+package ru.job4j.codewars;
+
+public class Money {
+    public static int calculateYears(double principal, double interest,  double tax, double desired) {
+        int result = 0;
+        while (principal < desired) {
+            principal += (principal * interest) -  ((principal * interest) * tax);
+            result++;
+        }
+        return result;
+    }
+}
