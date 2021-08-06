af5a74b51453e39f3c089a863dca6d195a0db544
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-06 17:38:34 +0300

    Gravity Flip

---
+++ src/main/java/ru/job4j/codewars/Flip.java
@@ -0,0 +1,14 @@
+package ru.job4j.codewars;
+
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Flip {
+    public static int[] flip(char dir, int[] arr) {
+        return Arrays.stream(arr).boxed()
+                .sorted(dir == 'L' ? (a, b) -> b - a : Comparator.comparingInt(a -> a))
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
