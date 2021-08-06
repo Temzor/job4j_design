999e6cf8bf541f2ceebadfd8ab931dd37f71988f
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-06 16:37:22 +0300

    Volume of a Cuboid

---
+++ src/main/java/ru/job4j/codewars/GetVolumeOfCuboid.java
@@ -0,0 +1,7 @@
+package ru.job4j.codewars;
+
+public class GetVolumeOfCuboid {
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        return length * width * height;
+    }
+}
