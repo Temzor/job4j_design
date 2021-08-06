9c844ea4e7949dea43291124d3d4c82a76a7e1ee
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-06 16:33:42 +0300

    Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence

---
+++ src/main/java/ru/job4j/codewars/Replace.java
@@ -0,0 +1,7 @@
+package ru.job4j.codewars;
+
+public class Replace {
+    public static String replace(final String s) {
+        return s.replaceAll("[aeiouAEIOU]", "!");
+    }
+}
---
+++ src/test/java/ru/job4j/codewars/ReplaceTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReplaceTest {
+    @Test
+    public void basicTests() {
+        assertEquals("H!!",      Replace.replace("Hi!"));
+        assertEquals("!H!! H!!", Replace.replace("!Hi! Hi!"));
+        assertEquals("!!!!!",    Replace.replace("aeiou"));
+        assertEquals("!BCD!",    Replace.replace("ABCDE"));
+    }
+
+}
