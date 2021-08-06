2a4d6b07faefb4f6cf19443e4b683c504ae224e9
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-06 17:05:55 +0300

    Multiplication table for number

---
+++ src/test/java/ru/job4j/codewars/MultiTableTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.codewars;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MultiTableTest {
+    @Test
+    public void basicTests() {
+        assertEquals(5, 5);
+        assertEquals(1, 1);
+    }
+
+}
