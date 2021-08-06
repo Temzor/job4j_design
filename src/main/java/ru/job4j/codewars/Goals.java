903882fefc3d402eb82fcdf6750c5dba584ace0f
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-06 17:15:10 +0300

    Grasshopper - Messi goals function

---
+++ src/main/java/ru/job4j/codewars/Goals.java
@@ -0,0 +1,7 @@
+package ru.job4j.codewars;
+
+public class Goals {
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+        return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    }
+}
