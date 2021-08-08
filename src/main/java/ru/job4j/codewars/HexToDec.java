5a111929e8c3ef8ef83ea3c3bfd8ccb0ad35e34d
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-08 22:25:33 +0300

    Hex to Decimal

---
+++ src/main/java/ru/job4j/codewars/HexToDec.java
@@ -0,0 +1,7 @@
+package ru.job4j.codewars;
+
+public class HexToDec {
+    public static int hexToDec(final String hexString) {
+        return Integer.parseInt(hexString, 16);
+    }
+}
---
+++ src/test/java/ru/job4j/codewars/HexToDecTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HexToDecTest {
+    @Test
+    public void examples() {
+        // assertEquals("expected", "actual");
+        assertEquals(1, HexToDec.hexToDec("1"));
+        assertEquals(10, HexToDec.hexToDec("a"));
+        assertEquals(16, HexToDec.hexToDec("10"));
+        assertEquals(255, HexToDec.hexToDec("FF"));
+        assertEquals(-12, HexToDec.hexToDec("-C"));
+    }
+
+}
