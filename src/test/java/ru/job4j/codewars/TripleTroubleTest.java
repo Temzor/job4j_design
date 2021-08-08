ced6bc93a155016e73ca33461699f340d3f510aa
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-08 22:18:49 +0300

    Triple Trouble

---
+++ src/test/java/ru/job4j/codewars/TripleTroubleTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TripleTroubleTest {
+    @Test
+    public void tests() {
+        assertEquals("ttlheoiscstk", TripleTrouble.tripleTrouble("this", "test", "lock"));
+        assertEquals("abcabc", TripleTrouble.tripleTrouble("aa", "bb", "cc"));
+        assertEquals("Batman", TripleTrouble.tripleTrouble("Bm", "aa", "tn"));
+        assertEquals("LexLuthor", TripleTrouble.tripleTrouble("LLh", "euo", "xtr"));
+    }
+
+}
