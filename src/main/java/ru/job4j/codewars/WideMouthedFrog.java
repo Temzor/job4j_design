678e0a09f72f74f4f4ee236b65a0907b1a78d74d
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-08 22:37:06 +0300

    The Wide-Mouthed frog!

---
+++ src/main/java/ru/job4j/codewars/WideMouthedFrog.java
@@ -0,0 +1,7 @@
+package ru.job4j.codewars;
+
+public class WideMouthedFrog {
+    public static String mouthSize(String animal) {
+        return animal.equalsIgnoreCase("alligator") ? "small" : "wide";
+    }
+}
---
+++ src/test/java/ru/job4j/codewars/WideMouthedFrogTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WideMouthedFrogTest {
+    @Test
+    public void fixedTests() {
+        assertEquals("wide", WideMouthedFrog.mouthSize("toucan"));
+        assertEquals("wide", WideMouthedFrog.mouthSize("ant bear"));
+        assertEquals("small", WideMouthedFrog.mouthSize("alligator"));
+    }
+}
