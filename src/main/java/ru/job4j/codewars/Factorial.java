019e53b800c75c0f5798199a2df7bd4f0a6ee654
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-09 22:59:36 +0300

    Factorial

---
+++ src/main/java/ru/job4j/codewars/Factorial.java
@@ -0,0 +1,11 @@
+package ru.job4j.codewars;
+
+public class Factorial {
+    public int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
