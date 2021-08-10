f6b41ffd12a30778b99cdfb91d8124f8ed5907ea
Temzor <onewaykd@gmail.com>
2021-08-10 15:54:15 +0300

    Round up to the next multiple of 5

---
+++ src/test/java/ru/job4j/codewars/RoundToTheNextMultipleOf5Test.java
@@ -0,0 +1,28 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class RoundToTheNextMultipleOf5Test {
+    @Test
+    public void basicTests() {
+        int[][] arr = {
+                {0, 0},
+                {1, 5},
+                {3, 5},
+                {5, 5},
+                {7, 10},
+                {39, 40}
+        };
+        Arrays.stream(arr)
+                .forEach(
+                        (testCase) -> assertEquals(
+                                "Input: " + testCase[0],
+                                testCase[1],
+                                RoundToTheNextMultipleOf5.roundToNext5(testCase[0])));
+    }
+
+}
