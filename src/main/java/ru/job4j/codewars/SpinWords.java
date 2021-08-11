ba407f92cb6a58eab3bca80a4f6948e5339c60ff
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-11 21:44:47 +0300

    Stop gninnipS My sdroW!

---
+++ src/main/java/ru/job4j/codewars/SpinWords.java
@@ -0,0 +1,10 @@
+package ru.job4j.codewars;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class SpinWords {
+    public String spinWords(String sentence) {
+        return Arrays.stream(sentence.split(" ")).map(x -> x.length() >= 5 ? new StringBuilder(x).reverse().toString() : x).collect(Collectors.joining(" "));
+    }
+}
