5df47155df71eb739004b190f60483eefdd10e3c
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-11 21:33:42 +0300

    Fix string case

---
+++ src/test/java/ru/job4j/codewars/SolveTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolveTest {
+    @Test
+    public void bsicTests() {
+        assertEquals("code", Solve.solve("code"));
+        assertEquals("CODE", Solve.solve("CODe"));
+        assertEquals("code", Solve.solve("COde"));
+        assertEquals("code", Solve.solve("Code"));
+        assertEquals("", Solve.solve(""));
+    }
+
+}
