1448702f3a33a32bf8ae66edf17a453a9f632ae0
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-13 20:19:08 +0300

    Take a Ten Minute Walk

---
+++ src/test/java/ru/job4j/codewars/TenMinWalkTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TenMinWalkTest {
+    @Test
+    public void test() {
+        assertTrue("Should return true", TenMinWalk.isValid(new char[]{'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}));
+        assertFalse("Should return false", TenMinWalk.isValid(new char[]{'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e'}));
+        assertFalse("Should return false", TenMinWalk.isValid(new char[]{'w'}));
+        assertFalse("Should return false", TenMinWalk.isValid(new char[]{'n', 'n', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}));
+    }
+
+}
