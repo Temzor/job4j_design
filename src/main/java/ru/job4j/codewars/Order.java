202980f56493033e494c6406ef2abe0bd540bb0a
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-14 14:08:20 +0300

    Your order, please

--- src/main/java/ru/job4j/codewars/Order.java
+++ src/main/java/ru/job4j/codewars/Order.java
@@ -4,16 +4,9 @@ import java.util.*;
 
 public class Order {
     public static String order(String words) {
-<<<<<<< HEAD
         System.out.println(8 & 16);
         return Arrays.stream(words.split(" "))
                 .sorted(Comparator.comparing(s -> Integer.valueOf(s.replaceAll("\\D", ""))))
                 .reduce((a, b) -> a + " " + b).get();
-
-=======
-        return Arrays.stream(words.split(" "))
-                .sorted(Comparator.comparing(s -> Integer.valueOf(s.replaceAll("\\D", ""))))
-                .reduce((a, b) -> a + " " + b).get();
->>>>>>> origin/master
     }
 }
