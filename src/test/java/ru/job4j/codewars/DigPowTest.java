e10b102a6cbfda84703642f5241effdf71a4c30d
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-16 22:00:29 +0300

    Playing with digits

---
+++ src/test/java/ru/job4j/codewars/DigPowTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DigPowTest {
+    @Test
+    public void test1() {
+        assertEquals(1, DigPow.digPow(89, 1));
+    }
+    @Test
+    public void test2() {
+        assertEquals(-1, DigPow.digPow(92, 1));
+    }
+    @Test
+    public void test3() {
+        assertEquals(51, DigPow.digPow(46288, 3));
+    }
+
+}
