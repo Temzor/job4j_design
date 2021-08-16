de855e122f5f73b27ba2b260514ba5cda160ae26
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-16 21:48:01 +0300

    Tribonacci Sequence

---
+++ src/main/java/ru/job4j/codewars/Xbonacci.java
@@ -0,0 +1,13 @@
+package ru.job4j.codewars;
+
+import java.util.Arrays;
+
+public class Xbonacci {
+    public double[] tribonacci(double[] s, int n) {
+        s = Arrays.copyOf(s, n);
+        for (var i = 3; i < n; i++) {
+            s[i] = s[i - 1] + s[i - 2] + s[i - 3];
+        }
+        return s;
+    }
+}
