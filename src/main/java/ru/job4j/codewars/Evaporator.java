3bd54da098ad0ab426f954f12850bb900b07d863
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-16 21:43:10 +0300

    Deodorant Evaporator

---
+++ src/main/java/ru/job4j/codewars/Evaporator.java
@@ -0,0 +1,13 @@
+package ru.job4j.codewars;
+
+public class Evaporator {
+    public static int evaporator(double content, double evapPerDay, double threshold) {
+        int result = 0;
+        final double constanta = content;
+        while (content > constanta * threshold / 100) {
+            content -= content * evapPerDay / 100;
+            result++;
+        }
+        return result;
+    }
+}
