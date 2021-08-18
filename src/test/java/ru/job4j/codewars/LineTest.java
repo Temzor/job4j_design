f4344373512611ba365a26141cd77c15c1de6026
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-18 18:28:47 +0300

    Vasya - Clerk

---
+++ src/test/java/ru/job4j/codewars/LineTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LineTest {
+    @Test
+    public void test1() {
+        assertEquals("YES", Line.tickets(new int[] {25, 25, 50}));
+    }
+    @Test
+    public void test2() {
+        assertEquals("NO", Line.tickets(new int[] {25, 100}));
+    }
+
+}
