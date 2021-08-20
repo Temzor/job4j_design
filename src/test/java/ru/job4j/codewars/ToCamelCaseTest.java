9ba33c6dfbb4d212f8d00a0ade5222cd94d21b98
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-20 20:45:17 +0300

    Convert string to camel case

---
+++ src/test/java/ru/job4j/codewars/ToCamelCaseTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ToCamelCaseTest {
+    @Test
+    public void testSomeUnderscoreLowerStart() {
+        String input = "the_Stealth_Warrior";
+        System.out.println("input: " + input);
+        assertEquals("theStealthWarrior", ToCamelCase.toCamelCase(input));
+    }
+    @Test
+    public void testSomeDashLowerStart() {
+        String input = "the-Stealth-Warrior";
+        System.out.println("input: " + input);
+        assertEquals("theStealthWarrior", ToCamelCase.toCamelCase(input));
+    }
+
+}
