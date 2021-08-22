db8b48b55b02b0c6f747bb967666a43dc18aee7f
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-22 15:44:05 +0300

    101 Dalmatians - squash the bugs, not the dogs!

--- src/main/java/ru/job4j/codewars/ZywOo2.java
+++ src/main/java/ru/job4j/codewars/ZywOo2.java
@@ -2,11 +2,7 @@ package ru.job4j.codewars;
 
 public class ZywOo2 {
     public static String howManyDalmations(int number) {
-
-    String[] dogs  = new String[] {"Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"};
-
-    return number <= 10 ? dogs[0] : (number <= 50 ? dogs[1] : (number == 101 ? dogs[3] : dogs[2]));
-
-}
+        return number <= 10 ? "Hardly any" : number <= 50 ? "More than a handful!" : number == 101 ? "101 DALMATIANS!!!" : "Woah that's a lot of dogs!";
+    }
 }
 
