6d65a32716f8c2badaf9aeb31efa16f3c1815065
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-22 16:19:15 +0300

    Palindrome chain length

---
+++ src/main/java/ru/job4j/codewars/Palindromes.java
@@ -0,0 +1,8 @@
+package ru.job4j.codewars;
+
+public class Palindromes {
+    public static int palindromeChainLength(long n) {
+        String ns = "" + n, nrs = "" + new StringBuilder(ns).reverse();
+        return ns.equals(nrs) ? 0 : 1 + palindromeChainLength(n + Long.parseLong(nrs));
+    }
+}
