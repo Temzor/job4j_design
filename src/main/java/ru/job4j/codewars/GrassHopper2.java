1aea5ee3920af283284245d07cc7da7c99de2e3b
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-26 15:16:05 +0300

    Grasshopper - Debug sayHello

---
+++ src/main/java/ru/job4j/codewars/GrassHopper2.java
@@ -0,0 +1,7 @@
+package ru.job4j.codewars;
+
+public class GrassHopper2 {
+    public static String sayHello(String name) {
+        return "Hello, " + name;
+    }
+}
---
+++ src/test/java/ru/job4j/codewars/GrassHopper2Test.java
@@ -0,0 +1,29 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GrassHopper2Test {
+    @Test
+    public void test1() {
+        assertEquals("Hello, Mr. Spock",
+                GrassHopper2.sayHello("Mr. Spock"));
+    }
+    @Test
+    public void test2() {
+        assertEquals("Hello, Captain Kirk",
+                GrassHopper2.sayHello("Captain Kirk"));
+    }
+    @Test
+    public void test3() {
+        assertEquals("Hello, Liutenant Uhura",
+                GrassHopper2.sayHello("Liutenant Uhura"));
+    }
+    @Test
+    public void test4() {
+        assertEquals("Hello, Dr. McCoy",
+                GrassHopper2.sayHello("Dr. McCoy"));
+    }
+
+}
