d40c8f5e95d3c5db6e86177aa752251de9542d31
Капустин Дмитрий <onewaykd@gmail.com>
2021-08-27 13:43:02 +0300

    Sum of numbers from 0 to N

---
+++ src/main/java/ru/job4j/codewars/SequenceSum.java
@@ -0,0 +1,20 @@
+package ru.job4j.codewars;
+
+public class SequenceSum {
+    public static String showSequence(int value) {
+        StringBuilder result = new StringBuilder();
+        if (value < 0) {
+            return   value + "<0";
+        } else if (value == 0) {
+            return "0=0";
+        } else {
+            int sum = 0;
+            for (int i = 0; i < value + 1; i++) {
+                result.append("+").append(i);
+                sum += i;
+            }
+            return result.toString().replaceFirst("\\+", "") + " = " + sum;
+
+        }
+    }
+}
