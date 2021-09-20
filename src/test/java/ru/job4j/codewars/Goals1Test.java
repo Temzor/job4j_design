2d5d1df66299bbe74ccbb15e29900c45e40699e8
Капустин Дмитрий <onewaykd@gmail.com>
2021-09-20 17:59:20 +0300

    Grasshopper - Messi Goals

---
+++ src/test/java/ru/job4j/codewars/Goals1Test.java
@@ -0,0 +1,13 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Goals1Test {
+    @Test
+    public void basicTests() {
+        assertEquals(58, Goals1.totalGoals);
+    }
+
+}
