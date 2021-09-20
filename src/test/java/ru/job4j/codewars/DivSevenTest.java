9258d487e04d6ad03ed5400a53523f2fc395e76c
Капустин Дмитрий <onewaykd@gmail.com>
2021-09-20 18:14:50 +0300

    A Rule of Divisibility by 7

---
+++ src/test/java/ru/job4j/codewars/DivSevenTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DivSevenTest {
+    @Test
+    public void test1() {
+        System.out.println("Basic Tests");
+        assertArrayEquals(new long[] {7, 2}, DivSeven.seven(1603));
+        assertArrayEquals(new long[] {35, 1}, DivSeven.seven(371));
+        assertArrayEquals(new long[] {42, 1}, DivSeven.seven(483));
+    }
+
+}
