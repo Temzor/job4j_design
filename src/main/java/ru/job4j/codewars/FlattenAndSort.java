61d5d89ed9a2a5ddb583a113a1814c86ef3178f2
Капустин Дмитрий <onewaykd@gmail.com>
2021-09-23 11:54:23 +0300

    Flatten and sort an array

---
+++ src/main/java/ru/job4j/codewars/FlattenAndSort.java
@@ -0,0 +1,10 @@
+package ru.job4j.codewars;
+
+import java.util.Arrays;
+
+
+public class FlattenAndSort {
+    public static int[] flattenAndSort(int[][] array) {
+        return Arrays.stream(array).flatMapToInt(Arrays::stream).sorted().toArray();
+    }
+}
