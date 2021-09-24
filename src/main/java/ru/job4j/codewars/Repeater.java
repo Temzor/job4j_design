263d636eb10aa7a640cafd4f24fd6edd351a7945
Капустин Дмитрий <onewaykd@gmail.com>
2021-09-24 18:49:31 +0300

    Thinkful - String Drills: Repeater

---
+++ src/main/java/ru/job4j/codewars/Repeater.java
@@ -0,0 +1,11 @@
+package ru.job4j.codewars;
+
+public class Repeater {
+    public static String repeat(String string, long n) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            result.append(string);
+        }
+        return result.toString();
+    }
+}
