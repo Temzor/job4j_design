2c0c37f8883a6a48f8245c1ffcb96b75b6f508a4
Капустин Дмитрий <onewaykd@gmail.com>
2021-09-25 22:43:45 +0300

    Grasshopper - Debug

---
+++ src/test/java/ru/job4j/codewars/GrassHopper3Test.java
@@ -0,0 +1,17 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GrassHopper3Test {
+    @Test
+    public void testA() {
+        assertEquals("10.0 is above freezing temperature", GrassHopper3.weatherInfo(50));
+    }
+    @Test
+    public void testB() {
+        assertEquals("-5.0 is freezing temperature", GrassHopper3.weatherInfo(23));
+    }
+
+}
