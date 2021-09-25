315269315bf6f912baada2baf94b8c31c7131f8b
Капустин Дмитрий <onewaykd@gmail.com>
2021-09-26 00:13:49 +0300

    Is it a number?

---
+++ src/main/java/ru/job4j/codewars/MyUtilities.java
@@ -0,0 +1,15 @@
+package ru.job4j.codewars;
+
+import java.lang.*;
+
+public class MyUtilities {
+
+    public boolean isDigit(String s) {
+        try {
+            Double.parseDouble(s);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
