78f9332b37f87cac7b3944e1e530b54eec9d2afb
Капустин Дмитрий <onewaykd@gmail.com>
2021-09-26 21:32:03 +0300

    Simple remove duplicates

---
+++ src/main/java/ru/job4j/codewars/Solve3.java
@@ -0,0 +1,15 @@
+package ru.job4j.codewars;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Solve3 {
+    public static int[] solve(int[] arr) {
+        Set<Integer> set = new LinkedHashSet<>();
+        for (int j : arr) {
+            set.remove(j);
+            set.add(j);
+        }
+        return set.stream().mapToInt(x -> x).toArray();
+    }
+}
