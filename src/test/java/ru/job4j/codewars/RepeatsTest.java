ead2184e5771ea941e29271e224afaa0ab91d827
Капустин Дмитрий <onewaykd@gmail.com>
2021-09-28 23:53:40 +0300

    Sum of array singles

---
+++ src/test/java/ru/job4j/codewars/RepeatsTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RepeatsTest {
+    @Test
+    public void basicTests() {
+        assertEquals(15, Repeats.repeats(new int[]{4, 5, 7, 5, 4, 8}));
+        assertEquals(19, Repeats.repeats(new int[]{9, 10, 19, 13, 19, 13}));
+        assertEquals(12, Repeats.repeats(new int[]{16, 0, 11, 4, 8, 16, 0, 11}));
+    }
+
+}
