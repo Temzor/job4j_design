1c39b471940ea9d50a9e79cf3f1ad7b501f66eda
Капустин Дмитрий <onewaykd@gmail.com>
2021-09-29 20:24:42 +0300

    Sum of differences in array

---
+++ src/test/java/ru/job4j/codewars/ZywOo5Test.java
@@ -0,0 +1,20 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ZywOo5Test {
+    @Test
+    public void basicTests() {
+        assertEquals(9, ZywOo5.sumOfDifferences(new int[]{1, 2, 10}));
+        assertEquals(2, ZywOo5.sumOfDifferences(new int[]{-3, -2, -1}));
+        assertEquals(0, ZywOo5.sumOfDifferences(new int[]{1, 1, 1, 1, 1}));
+        assertEquals(34, ZywOo5.sumOfDifferences(new int[]{-17, 17}));
+        assertEquals(0, ZywOo5.sumOfDifferences(new int[0]));
+        assertEquals(0, ZywOo5.sumOfDifferences(new int[]{0}));
+        assertEquals(0, ZywOo5.sumOfDifferences(new int[]{-1}));
+        assertEquals(0, ZywOo5.sumOfDifferences(new int[]{1}));
+    }
+
+}
