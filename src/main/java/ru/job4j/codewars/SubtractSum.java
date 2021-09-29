8ad3bb3bbc8e2303a1359198655cc58982154454
Капустин Дмитрий <onewaykd@gmail.com>
2021-09-29 20:07:18 +0300

    Subtract the Sum

---
+++ src/main/java/ru/job4j/codewars/SubtractSum.java
@@ -0,0 +1,42 @@
+package ru.job4j.codewars;
+
+import java.util.Arrays;
+
+public class SubtractSum {
+    public static String subtractSum(int n) {
+        String[] words = new String[] {
+                "kiwi", "pear", "kiwi", "banana", "melon",
+                "banana", "melon", "pineapple", "apple",
+                "pineapple", "cucumber", "pineapple",
+                "cucumber", "orange", "grape", "orange",
+                "grape", "apple", "grape", "cherry", "pear",
+                "cherry", "pear", "kiwi", "banana", "kiwi",
+                "apple", "melon", "banana", "melon",
+                "pineapple", "melon", "pineapple",
+                "cucumber", "orange", "apple", "orange",
+                "grape", "orange", "grape", "cherry",
+                "pear", "cherry", "pear", "apple", "pear",
+                "kiwi", "banana", "kiwi", "banana",
+                "melon", "pineapple", "melon",
+                "apple", "cucumber", "pineapple", "cucumber",
+                "orange", "cucumber", "orange", "grape",
+                "cherry", "apple", "cherry", "pear", "cherry",
+                "pear", "kiwi", "pear", "kiwi", "banana",
+                "apple", "banana", "melon", "pineapple",
+                "melon", "pineapple", "cucumber", "pineapple",
+                "cucumber", "apple", "grape", "orange", "grape",
+                "cherry", "grape", "cherry", "pear", "cherry",
+                "apple", "kiwi", "banana", "kiwi", "banana", "melon",
+                "banana", "melon", "pineapple", "apple", "pineapple"
+        };
+
+        do {
+            n -= Arrays
+                    .stream(String.valueOf(n).split(""))
+                    .mapToInt(Integer::parseInt)
+                    .sum();
+        } while (n > 100);
+        return words[n - 1];
+
+        }
+}
---
+++ src/test/java/ru/job4j/codewars/SubtractSumTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SubtractSumTest {
+    @Test
+    public void basicTest() {
+        assertEquals("apple", SubtractSum.subtractSum(10));
+    }
+
+}
