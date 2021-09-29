778276cb8a57a179b3cf6b04982a4b99c1ff57f0
Капустин Дмитрий <onewaykd@gmail.com>
2021-09-29 20:17:15 +0300

    Area of a Square

---
+++ src/main/java/ru/job4j/codewars/Geometry.java
@@ -0,0 +1,8 @@
+package ru.job4j.codewars;
+
+public class Geometry {
+    public static double squareArea(double a) {
+        double s = Math.pow(2 * a / Math.PI, 2);
+        return Math.round(s * 100) / 100.0;
+    }
+}
