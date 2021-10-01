3b90f78ace979b6b4704a086f61fa4ee77a5117e
Капустин Дмитрий <onewaykd@gmail.com>
2021-10-01 21:21:43 +0300

    Thinkful - Number Drills: Pixelart planning

---
+++ src/main/java/ru/job4j/codewars/IsDivisible2.java
@@ -0,0 +1,7 @@
+package ru.job4j.codewars;
+
+public class IsDivisible2 {
+    public static boolean isDivisible(int wallLength, int pixelSize) {
+        return wallLength % pixelSize == 0;
+    }
+}
---
+++ src/test/java/ru/job4j/codewars/IsDivisible2Test.java
@@ -0,0 +1,17 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IsDivisible2Test {
+    @Test
+    public void fixedTests() {
+        assertTrue(IsDivisible2.isDivisible(4050, 27));
+        assertTrue(IsDivisible2.isDivisible(10000, 20));
+        assertTrue(IsDivisible2.isDivisible(10005, 1));
+        assertFalse(IsDivisible2.isDivisible(4066, 27));
+        assertFalse(IsDivisible2.isDivisible(10005, 2));
+    }
+
+}
