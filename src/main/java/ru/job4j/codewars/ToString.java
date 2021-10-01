97165bf3c2a3620a1a111f6b096dff20ae853349
Капустин Дмитрий <onewaykd@gmail.com>
2021-10-01 21:29:52 +0300

    Number toString

---
+++ src/main/java/ru/job4j/codewars/ToString.java
@@ -0,0 +1,5 @@
+package ru.job4j.codewars;
+
+public class ToString {
+    public static final String A = Integer.toString(123);
+}
---
+++ src/test/java/ru/job4j/codewars/ToStringTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ToStringTest {
+    @Test
+    public void tests() {
+        assertEquals("Wrong!", ToString.A, "123");
+    }
+
+}
