4d25f557c04484643f8b7d9b45101eec313dde65
Капустин Дмитрий <onewaykd@gmail.com>
2021-10-01 21:50:08 +0300

    Miles per gallon to kilometers per liter

---
+++ src/main/java/ru/job4j/codewars/Converter.java
@@ -0,0 +1,7 @@
+package ru.job4j.codewars;
+
+public class Converter {
+    public static float mpgToKPM(final float mpg) {
+        return (float) (Math.round(((mpg / 4.54609188f) * 1.609344f) * 100.0) / 100.0);
+    }
+}
