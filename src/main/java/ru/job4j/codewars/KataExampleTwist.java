8e6445546df868703141445af8ef8b6e0d2c4e38
Капустин Дмитрий <onewaykd@gmail.com>
2021-10-04 21:07:56 +0300

    Kata Example Twist

---
+++ src/main/java/ru/job4j/codewars/KataExampleTwist.java
@@ -0,0 +1,10 @@
+package ru.job4j.codewars;
+
+import java.util.Collections;
+
+public class KataExampleTwist {
+    public static String[] kataExampleTwist() {
+        String[] websites = {};
+        return Collections.nCopies(1000, "codewars").toArray(websites);
+    }
+}
