6b317c204596ea56e968fce6ca78c84e11ef0c2b
Капустин Дмитрий <onewaykd@gmail.com>
2021-10-04 20:22:22 +0300

    Sleigh Authentication

---
+++ src/test/java/ru/job4j/codewars/SleighTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SleighTest {
+    @Test
+    public void testAuthentication() {
+        assertEquals(true, Sleigh.authenticate("Santa Claus", "Ho Ho Ho!"));
+        assertEquals(false, Sleigh.authenticate("Santa", "Ho Ho Ho!"));
+        assertEquals(false, Sleigh.authenticate("Santa Claus", "Ho Ho Ho"));
+    }
+
+}
