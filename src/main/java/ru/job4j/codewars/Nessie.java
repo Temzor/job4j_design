7318ae1ff7b831a8262262d81eeb950097033255
Капустин Дмитрий <onewaykd@gmail.com>
2021-10-04 20:26:47 +0300

    return s.contains("tree fiddy") || s.contains("3.50") || s.contains("three fifty");

---
+++ src/main/java/ru/job4j/codewars/Nessie.java
@@ -0,0 +1,7 @@
+package ru.job4j.codewars;
+
+public class Nessie {
+    public static boolean isLockNessMonster(String s) {
+        return s.contains("tree fiddy") || s.contains("3.50") || s.contains("three fifty");
+    }
+}
