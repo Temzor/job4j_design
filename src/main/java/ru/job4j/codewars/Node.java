16fd7123217eb1f8f6c17faf91d2a5389b1d3ffd
Капустин Дмитрий <onewaykd@gmail.com>
2021-10-08 10:32:52 +0300

    Convert a linked list to a string

---
+++ src/main/java/ru/job4j/codewars/Node.java
@@ -0,0 +1,24 @@
+package ru.job4j.codewars;
+
+class Node {
+    private int data;
+    private Node next;
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+}
