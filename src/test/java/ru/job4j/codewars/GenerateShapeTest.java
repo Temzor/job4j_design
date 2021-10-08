8f87da59e416452ffec92aa937fefc12dbc28665
Капустин Дмитрий <onewaykd@gmail.com>
2021-10-08 10:23:57 +0300

    Build a square

---
+++ src/test/java/ru/job4j/codewars/GenerateShapeTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GenerateShapeTest {
+    @Test
+    public void exampleTests() {
+        assertEquals("+++\n+++\n+++", GenerateShape.generateShape(3));
+        assertEquals("+++++\n+++++\n+++++\n+++++\n+++++", GenerateShape.generateShape(5));
+        assertEquals("++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++", GenerateShape.generateShape(8));
+    }
+}
