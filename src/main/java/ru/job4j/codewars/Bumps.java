797173a745f062a0b694530380059af159e015a2
Капустин Дмитрий <onewaykd@gmail.com>
2021-10-11 21:55:46 +0300

    Bumps in the Road

--- src/main/java/ru/job4j/codewars/Bumps.java
+++ src/main/java/ru/job4j/codewars/Bumps.java
@@ -2,13 +2,6 @@ package ru.job4j.codewars;
 
 public class Bumps {
     public static String bumps(final String road) {
-        char someChar = 'n';
-        int count = 0;
-        for (int i = 0; i < road.length(); i++) {
-            if (road.charAt(i) == someChar) {
-                count++;
-            }
-        }
-        return count >= 15 ? "Car Dead" : "Woohoo!";
+        return road.chars().filter(ch -> ch == 'n').count() > 15 ? "Car Dead" : "Woohoo!";
     }
 }
