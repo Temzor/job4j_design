afed1276631515d5058fddeebb735516e20ed087
Капустин Дмитрий <onewaykd@gmail.com>
2021-10-15 20:18:47 +0300

    Merging sorted integer arrays (without duplicates)

---
+++ src/main/java/ru/job4j/codewars/MergeArrays.java
@@ -0,0 +1,15 @@
+package ru.job4j.codewars;
+
+
+import java.util.Arrays;
+
+public class MergeArrays {
+    public static int[] mergeArrays(int[] first, int[] second) {
+        int[] result = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return Arrays.stream(result)
+                .sorted()
+                .distinct()
+                .toArray();
+    }
+}
