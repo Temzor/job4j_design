e05e45b258874355aaa0a15f219e50d568d8b417
Temzor <onewaykd@gmail.com>
2021-10-18 18:21:59 +0300

    Ghost code?!

---
+++ src/main/java/ru/job4j/codewars/GhostCode.java
@@ -0,0 +1,10 @@
+package ru.job4j.codewars;
+
+public class GhostCode {
+    public static String helloName(final String name){
+        if(name == null || name.equals(""))
+            return "Hello world!";
+        else
+            return "Hello my name is " + name;
+    }
+}
