ee491c2bb4c33be0b5d3361c85bddb059e65d940
Капустин Дмитрий <onewaykd@gmail.com>
2021-10-19 00:11:12 +0300

    refactor

---
+++ src/main/java/codewars/NoSpace.java
@@ -0,0 +1,7 @@
+package codewars;
+
+public class NoSpace {
+    public static String noSpace(final String x) {
+        return x.replace(" ", "");
+    }
+}
--- src/main/java/ru/job4j/codewars/NoSpace.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.codewars;
-
-public class NoSpace {
-    static String noSpace(final String x) {
-        return x.replace(" ", "");
-    }
-}
