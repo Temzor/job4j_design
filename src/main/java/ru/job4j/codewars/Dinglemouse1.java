de15f87d86ed0375320327a60898d95a01b593d1
Temzor <onewaykd@gmail.com>
2021-10-18 18:18:37 +0300

    FIXME: Static electrickery

---
+++ src/main/java/ru/job4j/codewars/Dinglemouse1.java
@@ -0,0 +1,24 @@
+package ru.job4j.codewars;
+
+public class Dinglemouse1 {
+
+    public static final Dinglemouse1 INST = new Dinglemouse1().setValueAndReturnInstance(100);
+
+    private static int ONE_HUNDRED = 100;
+
+    private int value;
+
+    private Dinglemouse1() {
+        value = ONE_HUNDRED;
+    }
+
+    public int plus100(int n) {
+        return value + n;
+    }
+
+    public Dinglemouse1 setValueAndReturnInstance (int n){
+        this.value=100;
+        return this;
+    }
+
+}
---
+++ src/test/java/ru/job4j/codewars/Dinglemouse1Test.java
@@ -0,0 +1,15 @@
+package ru.job4j.codewars;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Dinglemouse1Test {
+    @Test
+    public void test() {
+        assertEquals(123, Dinglemouse1.INST.plus100(23));
+        assertEquals(134, Dinglemouse1.INST.plus100(34));
+        assertEquals(64, Dinglemouse1.INST.plus100(-36));
+    }
+}
