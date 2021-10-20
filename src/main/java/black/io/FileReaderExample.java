ec0cac5c0ce11d40042923125c71cbf4462f2b7e
Капустин Дмитрий <onewaykd@gmail.com>
2021-10-20 16:10:29 +0300

    9. Работа с файлами IO и NIO 3. Try with resources

--- src/main/java/black/io/FileReaderExample.java
+++ src/main/java/black/io/FileReaderExample.java
@@ -1,11 +1,13 @@
 package black.io;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileReaderExample {
     public static void main(String[] args) throws IOException {
-        try (FileReader reader = new FileReader("TestRubai.txt")) {
+        try (FileReader reader = new FileReader("TestRubai.txt");
+        FileWriter writer = new FileWriter("TestRubai.txt", true)) {
             int character;
             while ((character = reader.read()) != -1) {
                 System.out.print((char) character);
