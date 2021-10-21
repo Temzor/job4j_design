6b943dcc590756703587e26ede9a19478fec8e0b
Капустин Дмитрий <onewaykd@gmail.com>
2021-10-21 13:35:46 +0300

    9. Работа с файлами IO и NIO 5. FileInputStream и FileOutputStream

---
+++ src/main/java/black/io/CopyExamplePicture.java
@@ -0,0 +1,21 @@
+package black.io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyExamplePicture {
+    public static void main(String[] args) {
+        try (FileInputStream inputStream = new FileInputStream("4dd8cb2342fb0b33903ef63ce932f17c.jpg");
+             FileOutputStream outputStream = new FileOutputStream("clone1.jpg")) {
+            int i;
+            while ((i = inputStream.read()) != -1) {
+                outputStream.write(i);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
