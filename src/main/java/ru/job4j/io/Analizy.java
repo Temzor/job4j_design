7a0111c826ea98684a73a974dab455fb8488239f
Капустин Дмитрий <onewaykd@gmail.com>
2021-10-25 18:53:23 +0300

    1. Читаем файл конфигурации [#858 #220337]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,38 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analizy {
+    public void unavailable(String source, String target) {
+        try (
+                BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new FileOutputStream(target))
+        ) {
+            boolean status200 = true;
+            while (in.ready()) {
+                String notice = in.readLine();
+                String[] data = notice.split(" ");
+                int status = Integer.parseInt(data[0]);
+                String date = data[1];
+                if ((status == 400 || status == 500) && status200) {
+                    out.print(date + ";");
+                    status200 = false;
+                } else if ((status == 200 || status == 300) && !status200) {
+                    out.println(date + ";");
+                    status200 = true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
+            out.println("15:01:30;15:02:32");
+            out.println("15:10:30;23:12:32");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
