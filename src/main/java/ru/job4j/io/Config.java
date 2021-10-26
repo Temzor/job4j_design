25d9909dfd4b00813bc9ab035d1b2dd54b145c08
Капустин Дмитрий <onewaykd@gmail.com>
2021-10-26 21:28:46 +0300

    1. Читаем файл конфигурации [#858 #220337]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,54 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            reader.lines().forEach(r -> {
+                if (!r.isEmpty() && !r.contains("#")) {
+                    String[] element = r.split("=");
+                    if (element.length != 2 || element[0].isEmpty()) {
+                        throw new IllegalArgumentException("Unknown element passed");
+                    }
+                    values.put(element[0], element[1]);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("C:\\projects\\job4j_design\\ app.properties"));
+    }
+
+}
