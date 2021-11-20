653f4b8222305da94e0b8ca3f76da24013a80026
Капустин Дмитрий <onewaykd@gmail.com>
2021-11-20 19:18:58 +0300

    3. Удалить head в односвязном списке. [#51424 #180669]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -19,15 +19,7 @@ public class ForwardLinked<T> implements Iterable<T> {
         tail.next = node;
     }
 
-    public void addFirst(T value) {
-        Node<T> nodeFist = new Node<>(value, null);
-        if (head != null) {
-            nodeFist.next = head;
-        }
-        head = nodeFist;
-    }
-
-   public T deleteFirst() {
+    public T deleteFirst() {
         if (head == null) {
             throw new NoSuchElementException("Element not found.");
         } else {
@@ -35,23 +27,9 @@ public class ForwardLinked<T> implements Iterable<T> {
             head = head.next;
             return result;
         }
-   }
-
-    public boolean revert() {
-        if (head == null || head.next == null) {
-            return false;
-        }
-        Node<T> previous = null;
-        Node<T> current = head;
-        while (current != null) {
-            Node<T> next = current.next;
-            current.next = previous;
-            previous = current;
-            current = next;
-        }
-        head = previous;
-        return true;
     }
+
+
 
     @Override
     public Iterator<T> iterator() {
@@ -66,16 +44,19 @@ public class ForwardLinked<T> implements Iterable<T> {
             @Override
             public T next() {
                 if (!hasNext()) {
-                    throw new NoSuchElementException();
+                    throw new NoSuchElementException("Element not found.");
                 }
                 T value = node.value;
+                node.value = null;
                 node = node.next;
                 return value;
             }
         };
     }
 
-      private static class Node<T> {
+
+
+    private static class Node<T> {
         T value;
         Node<T> next;
 
--- src/test/java/ru/job4j/collection/ForwardLinkedTest.java
+++ src/test/java/ru/job4j/collection/ForwardLinkedTest.java
@@ -1,47 +1,36 @@
 package ru.job4j.collection;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.is;
-
 import org.junit.Test;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ForwardLinkedTest {
 
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteFirst() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.deleteFirst();
+        linked.iterator().next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteEmptyLinked() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.deleteFirst();
+    }
+
     @Test
-    public void whenAddThenIter() {
+    public void whenMultiDelete() {
         ForwardLinked<Integer> linked = new ForwardLinked<>();
         linked.add(1);
         linked.add(2);
-        Iterator<Integer> it = linked.iterator();
-        assertThat(it.next(), is(1));
-        assertThat(it.next(), is(2));
-    }
-
-    @Test
-    public void whenAddAndRevertThenIter() {
-        ForwardLinked<Integer> linked = new ForwardLinked<>();
-        linked.add(1);
-        linked.add(2);
-        linked.revert();
+        assertThat(linked.deleteFirst(), is(1));
         Iterator<Integer> it = linked.iterator();
         assertThat(it.next(), is(2));
-        assertThat(it.next(), is(1));
     }
-
-    @Test
-    public void whenSize0ThenReturnFalse() {
-        ForwardLinked<Integer> emptyList = new ForwardLinked<>();
-        assertFalse(emptyList.revert());
-    }
-
-    @Test
-    public void whenSize1ThenReturnFalse() {
-        ForwardLinked<Integer> singleList = new ForwardLinked<>();
-        singleList.add(1);
-        assertFalse(singleList.revert());
-    }
-
 }
