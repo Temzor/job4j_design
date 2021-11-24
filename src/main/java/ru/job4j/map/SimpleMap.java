6e1dc6da084b1eb151aadde28cded320359e678e
Temzor <onewaykd@gmail.com>
2021-11-24 11:00:23 +0300

    Save code for education job4j: 8. Реализовать собственную структуру данных - HashMap [#1008 #189868]

--- src/main/java/ru/job4j/map/SimpleMap.java
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -1,15 +1,12 @@
 package ru.job4j.map;
 
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 
 public class SimpleMap<K, V> implements Map<K, V> {
 
     private static final float LOAD_FACTOR = 0.75f;
 
-    private int capacity = 1 << 4;
+    private int capacity = 8;
 
     private int count = 0;
 
@@ -19,85 +16,34 @@ public class SimpleMap<K, V> implements Map<K, V> {
 
     @Override
     public boolean put(K key, V value) {
-        if (count >= capacity * LOAD_FACTOR) {
-            expand();
-        }
-        int index = indexFor(hash(key.hashCode()));
-        if (table[index] == null) {
-            table[index] = new MapEntry(key, value);
-            count++;
-            modCount++;
-            return true;
-        }
         return false;
     }
 
-
     private int hash(int hashCode) {
-        return hashCode * 13 ^ hashCode >>> 12;
-        }
+        return 0;
+    }
 
     private int indexFor(int hash) {
-        return hash & (capacity - 1);
+        return 0;
     }
 
     private void expand() {
-        capacity *= 2;
-        MapEntry<K, V>[] temp = table;
-        table = new MapEntry[capacity];
-        for (int i = 0; i < capacity / 2; i++) {
-            if (temp[i] != null) {
-                table[indexFor(hash(temp[i].key.hashCode()))] = temp[i];
-            }
-        }
+
     }
 
     @Override
     public V get(K key) {
-        int index = indexFor(hash(key.hashCode()));
-        return table[index] != null && table[index].key.equals(key) ? table[index].value : null;
+        return null;
     }
 
     @Override
     public boolean remove(K key) {
-        int index = indexFor(hash(key.hashCode()));
-        if (table[index] != null && table[index].key.equals(key)) {
-            table[index] = null;
-            count--;
-            modCount++;
-            return true;
-        } else {
-            return false;
-        }
+        return false;
     }
 
     @Override
-    public Iterator iterator() {
-        return new Iterator<>() {
-            private int index = 0;
-            private int iteratorPoint = 0;
-            private final int expectedCountMod = modCount;
-
-            @Override
-            public boolean hasNext() {
-                return iteratorPoint < count;
-            }
-
-            @Override
-            public MapEntry<K, V> next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException("Element no found!");
-                }
-                if (expectedCountMod != modCount) {
-                    throw new ConcurrentModificationException("Wait, and try again!");
-                }
-                while (index < table.length && table[index] == null) {
-                    index++;
-                }
-                iteratorPoint++;
-                return table[index++];
-            }
-        };
+    public Iterator<K> iterator() {
+        return null;
     }
 
     private static class MapEntry<K, V> {
