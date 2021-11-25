e44eb97c509e3e4e4131119636f597692a1a5697
Temzor <onewaykd@gmail.com>
2021-11-25 11:16:02 +0300

    8. Реализовать собственную структуру данных - HashMap [#1008 #189868]

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,7 +1,6 @@
 package ru.job4j.tree;
 
 import java.util.*;
-import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
@@ -23,22 +22,13 @@ public class SimpleTree<E> implements Tree<E> {
     }
 
     @Override
-    public Optional<Tree.Node<E>> findBy(E value) {
-        return findByPredicate((x) -> x.value.equals(value));
-    }
-
-    public boolean isBinary() {
-        Optional<Tree.Node<E>> result = findByPredicate((x) -> x.children.size() > 2);
-        return result.isEmpty();
-    }
-
-    private Optional<Tree.Node<E>> findByPredicate(Predicate<Tree.Node<E>> condition) {
-        Optional<Tree.Node<E>> result = Optional.empty();
-        Queue<Tree.Node<E>> data = new LinkedList<>();
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> result = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
-            Tree.Node<E> el = data.poll();
-            if (condition.test(el)) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
                 result = Optional.of(el);
                 break;
             }
