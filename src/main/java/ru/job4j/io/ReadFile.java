4fb90183ddcbe5d1be5b2d281df9d06741177b31
Капустин Дмитрий <onewaykd@gmail.com>
2021-11-26 16:54:56 +0300

    0.3. BufferedReader. [#252489 #211738]

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -1,16 +1,15 @@
 package ru.job4j.io;
 
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class ReadFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("input.txt")) {
-            StringBuilder text = new StringBuilder();
-            int read;
-            while ((read = in.read()) != -1) {
-                text.append((char) read);
+        try (BufferedReader in = new BufferedReader(new FileReader("input.txt"))) {
+            in.lines().forEach(System.out::println);
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                System.out.println(line);
             }
-            System.out.println(text);
         } catch (Exception e) {
             e.printStackTrace();
         }
