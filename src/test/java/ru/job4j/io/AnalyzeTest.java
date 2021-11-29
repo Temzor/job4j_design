a2f567b2229622f016e9262ee96379175c6b0d30
Temzor <onewaykd@gmail.com>
2021-11-29 11:40:40 +0300

    2. Анализ доступности сервера. [#859 #222396]

--- src/test/java/ru/job4j/io/AnalyzeTest.java
+++ src/test/java/ru/job4j/io/AnalyzeTest.java
@@ -7,7 +7,7 @@ public class AnalyzeTest {
     @Test
     public void whenLogFile1() {
         String source = "source1.csv";
-        String target = "target1.txt";
+        String target = "targetResult1.txt";
         Analyze analyze = new Analyze();
         analyze.unavailable(source, target);
     }
@@ -15,7 +15,7 @@ public class AnalyzeTest {
     @Test
     public void whenLogFile2() {
         String source = "source2.csv";
-        String target = "target2.txt";
+        String target = "targetResult2.txt";
         Analyze analyze = new Analyze();
         analyze.unavailable(source, target);
     }
