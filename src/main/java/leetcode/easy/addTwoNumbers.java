603e325c91fc45ddac0822e657528f951482f4b1
Temzor <onewaykd@gmail.com>
2021-12-01 12:05:48 +0300

    LeetCode #2 - Add Two Numbers : Easy

---
+++ src/main/java/leetcode/easy/addTwoNumbers.java
@@ -0,0 +1,28 @@
+package leetcode.easy;
+
+
+public class addTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode res = new ListNode(0);
+        ListNode prev = res;
+        while(l1 != null | l2 != null)
+        {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int total = x + y + carry;
+            carry=total / 10;
+            ListNode node = new ListNode(total % 10);
+            res.next = node;
+            res = res.next;
+            if(l1 != null)
+                l1 = l1.next;
+            if(l2 != null)
+                l2 = l2.next;
+
+        }
+        if(carry > 0)
+            res.next = new ListNode(carry);
+        return prev.next;
+    }
+}
