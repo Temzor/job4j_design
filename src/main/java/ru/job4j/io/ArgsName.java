4c02e44af534c0a410020c07571165c6161fcf85
Капустин Дмитрий <onewaykd@gmail.com>
2021-12-02 13:02:21 +0300

    5.1. Именованные аргументы [#295518 #227107]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -12,13 +12,34 @@ public class ArgsName {
     }
 
     private void parse(String[] args) {
-        /* TODO parse args to values. */
+        if (!validatorLengthArray(args)) {
+            for (String arg : args) {
+                if (!validatorValueArray(arg)) {
+                    String[] pair = arg.split("=");
+                    values.put(pair[0].substring(1), pair[1]);
+                }
+            }
+        }
     }
 
     public static ArgsName of(String[] args) {
         ArgsName names = new ArgsName();
         names.parse(args);
         return names;
+    }
+
+    public static boolean validatorLengthArray(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Parameter is not exist, no found key to run, example /-Xmx=512/, /-encoding=UTF-8./");
+        }
+        return false;
+    }
+
+    public static boolean validatorValueArray(String arg) {
+        if (!arg.startsWith("-") || !arg.contains("=") || arg.startsWith("=") || arg.endsWith("=") || arg.startsWith("-=")) {
+            throw new IllegalArgumentException("Incorrect parameter " + arg + ". Usage -PARAMETER=VALUE.");
+        }
+        return false;
     }
 
     public static void main(String[] args) {
