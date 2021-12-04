11535f074a60adbac11d2b418b1dbfc21885653b
Капустин Дмитрий <onewaykd@gmail.com>
2021-12-04 17:28:04 +0300

    example 6. Кодировка. [#862]

--- src/main/java/ru/job4j/io/UsageEncoding.java
+++ src/main/java/ru/job4j/io/UsageEncoding.java
@@ -1,11 +1,13 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.charset.Charset;
+import java.util.List;
 
 public class UsageEncoding {
     public String readFile(String path) {
         StringBuilder builder = new StringBuilder();
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path, Charset.forName("WINDOWS-1251")))) {
             br.lines().forEach(builder::append);
         } catch (IOException e) {
             e.printStackTrace();
@@ -13,9 +15,9 @@ public class UsageEncoding {
         return builder.toString();
     }
 
-    public void writeDataInFile(String path, String data) {
-        try (PrintWriter pw = new PrintWriter(new FileWriter(path, true))) {
-            pw.println(data);
+    public void writeDataInFile(String path, List<String> data) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(path, Charset.forName("WINDOWS-1251"), true))) {
+            data.forEach(pw::println);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -24,6 +26,14 @@ public class UsageEncoding {
     public static void main(String[] args) {
         String path = "./data/text.txt";
         UsageEncoding encoding = new UsageEncoding();
+        List<String> strings = List.of(
+                "Новая строка 1",
+                "Новая строка 2",
+                "Новая строка 3",
+                "Новая строка 4",
+                "Новая строка 5"
+        );
+        encoding.writeDataInFile(path, strings);
         String s = encoding.readFile(path);
         System.out.println("Данные из файла: ");
         System.out.println(s);
