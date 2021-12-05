005fc8a007f42c15b7ee9b5ff1d376fb8dfb332d
Капустин Дмитрий <onewaykd@gmail.com>
2021-12-05 14:30:21 +0300

    Leetcode 14. Longest Common Prefix Easy

---
+++ src/main/java/leetcode/easy/LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+package leetcode.easy;
+
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strings) {
+        if (strings == null || strings.length == 0) {
+            return "";
+        }
+
+        Arrays.sort(strings);
+        String first = strings[0];
+        String last = strings[strings.length - 1];
+        int c = 0;
+        while (c < first.length()) {
+            if (first.charAt(c) == last.charAt(c)) {
+                c++;
+            } else {
+                break;
+            }
+        }
+        return c == 0 ? "" : first.substring(0, c);
+    }
+}
---
+++ src/test/java/leetcode/easy/LongestCommonPrefixTest.java
@@ -0,0 +1,23 @@
+package leetcode.easy;
+
+import junit.framework.TestCase;
+
+
+public class LongestCommonPrefixTest extends TestCase {
+
+    public void testOne() {
+        LongestCommonPrefix longestCommonPrefix  = new LongestCommonPrefix();
+        String[] in = new String[] {"flower", "flow", "flight"};
+        String result = longestCommonPrefix.longestCommonPrefix(in);
+        String expected  = "fl";
+        assertEquals(expected, result);
+    }
+
+    public void testTwo() {
+        LongestCommonPrefix longestCommonPrefix  = new LongestCommonPrefix();
+        String[] in = new String[] {"flower", "gow", "night"};
+        String result = longestCommonPrefix.longestCommonPrefix(in);
+        String expected  = "";
+        assertEquals(expected, result);
+    }
+}
