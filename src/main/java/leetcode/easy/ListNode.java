ba61fef0f1d30f81607f44ef948083d24a6f6333
Капустин Дмитрий <onewaykd@gmail.com>
2021-12-07 21:35:34 +0300

    LeetCode: 21. Merge Two Sorted Lists : Easy

--- src/main/java/leetcode/easy/ListNode.java
+++ src/main/java/leetcode/easy/ListNode.java
@@ -3,7 +3,11 @@ package leetcode.easy;
 public class ListNode {
     int val;
     ListNode next;
-    ListNode(int i, int i1, int i2) {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    ListNode(int i, int i1, int i2) { }
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val; this.next = next;
+    }
   }
