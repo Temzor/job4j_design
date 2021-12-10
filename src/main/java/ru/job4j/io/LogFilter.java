c4284541d44223042d160d2f1c89abcb24d25dcd
Капустин Дмитрий <onewaykd@gmail.com>
2021-12-10 21:57:49 +0300

    0.3. BufferedReader. [#252489 #211738]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -7,33 +7,27 @@ import java.util.stream.Collectors;
 
 public class LogFilter {
     public static List<String> filter(String file) {
-        List<String> resultLine = new ArrayList<>();
-        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            resultLine = in.lines()
-                    .filter(t -> t.contains(" 404 "))
-                    .collect(Collectors.toList());
-
-        }  catch (IOException e) {
+        List<String> result = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file));
+             PrintWriter out = new PrintWriter(new BufferedOutputStream(
+                     new FileOutputStream("resultLogFilter.txt")))) {
+           result = in.lines()
+                   .filter(t -> t.split(" ")[t.split(" ").length - 2].equals("404"))
+                   .collect(Collectors.toList());
+            for (String s : result) {
+                out.write(s);
+                out.write(System.lineSeparator());
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return resultLine;
+        return result;
     }
-
-    public static void save(List<String> log, String file) {
-        try (PrintWriter printWriter  = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(file)
-                ))) {
-            log.forEach(printWriter::println);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
 
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        save(log, "404.txt");
+        for (String s : log) {
+            System.out.println(s);
         }
     }
+}
