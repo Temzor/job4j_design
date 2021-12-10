391900f90f8c7ddd8285c1db5b010ba257ddee55
Капустин Дмитрий <onewaykd@gmail.com>
2021-12-10 22:13:07 +0300

    0.4. BufferedOutputStream [#252490 #217852]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -8,26 +8,29 @@ import java.util.stream.Collectors;
 public class LogFilter {
     public static List<String> filter(String file) {
         List<String> result = new ArrayList<>();
-        try (BufferedReader in = new BufferedReader(new FileReader(file));
-             PrintWriter out = new PrintWriter(new BufferedOutputStream(
-                     new FileOutputStream("resultLogFilter.txt")))) {
-           result = in.lines()
-                   .filter(t -> t.split(" ")[t.split(" ").length - 2].equals("404"))
-                   .collect(Collectors.toList());
-            for (String s : result) {
-                out.write(s);
-                out.write(System.lineSeparator());
-            }
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            result = in.lines()
+                    .filter(t -> t.contains(" 404 "))
+                    .collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
         return result;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(new FileOutputStream(file)))) {
+            log.forEach(out::println);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        for (String s : log) {
-            System.out.println(s);
-        }
+        save(log, "404.txt");
     }
 }
--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,16 +1,16 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
+import java.io.PrintWriter;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            for (int row = 1; row <= 10; row++) {
-                for (int cell = 1; cell <= 10; cell++) {
-                    out.write(((row * cell) + " | ").getBytes());
-                }
-                out.write(System.lineSeparator().getBytes());
-            }
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("result.txt")
+                ))) {
+            out.println("Hello, world!");
         } catch (Exception e) {
             e.printStackTrace();
         }
