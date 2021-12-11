578f38a50d81345786e1d2ebc6ae73a3bb6696e7
Капустин Дмитрий <onewaykd@gmail.com>
2021-12-11 11:47:04 +0300

    4.0. File [#252491 #225138]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -15,7 +15,7 @@ public class Dir {
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.printf("size disk: %s%n", file.getTotalSpace() / 1073741824  + " GB");
+        System.out.printf("size disk: %s%n", file.getTotalSpace() / 1073741824  + " GB.");
         for (File subfile : Objects.requireNonNull(file.listFiles())) {
             System.out.println("file: " + subfile.getName() + " | size = " + subfile.length() / 1024 + " Kb.");
         }
