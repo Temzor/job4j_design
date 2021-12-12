762b06cbc892abae2c606987d70a8d126d13a7c7
Капустин Дмитрий <onewaykd@gmail.com>
2021-12-12 12:58:37 +0300

    4.2. Поиск дубликатов [#315066 #225915]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -6,6 +6,8 @@ import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
+        DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("C:/test"), duplicatesVisitor);
+        duplicatesVisitor.getFiles().forEach(System.out::println);
     }
 }
