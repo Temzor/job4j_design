56684bd7ae78e8dd2e9e2b2043bb3a496e237ca3
Капустин Дмитрий <onewaykd@gmail.com>
2021-12-15 12:02:56 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,10 +1,14 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -35,6 +39,8 @@ public class EchoServer {
                     out.flush();
                 }
             }
+        } catch (Exception e) {
+            LOG.error("[ERROR:]", e);
         }
     }
 }
--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -10,17 +10,10 @@ public class UsageLog4j {
 
     public static void main(String[] args) {
         BasicConfigurator.configure();
-        byte maxByte = 127;
-        short maxShort = 32767;
-        char charH = 72;
-        int maxInt = 2147483647;
-        long maxLong = 9223372036854775807L;
-        float floatPiTwo = 3.14f;
-        double doublePiThree = 3.141;
-        boolean booleanTrue = true;
-
-
-        LOG.debug("Primitive types of Java, byte: {}, short : {}, char : {}, int : {}, long : {}, float : {}, double : {},  boolean : {}",
-                maxByte, maxShort, charH, maxInt, maxLong, floatPiTwo, doublePiThree, booleanTrue);
+        try {
+            throw new Exception("Not supported code");
+        } catch (Exception e) {
+            LOG.error("Exception in log example", e);
+        }
     }
 }
