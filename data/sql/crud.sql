0d77af1e78e530b7bfed895e3e3ca8cdd10791bd
Капустин Дмитрий <onewaykd@gmail.com>
2021-12-17 22:05:09 +0300

    0. Что такое база данных. [#6860]

---
+++ data/sql/crud.sql
@@ -0,0 +1,13 @@
+CREATE TABLE products (
+id serial PRIMARY KEY,
+createtime timestamp(6) NOT NULL,
+name text COLLATE "pg_catalog"."default",
+price int8 NOT NULL,
+mark bool NOT NULL DEFAULT false);
+
+INSERT INTO products(createtime, name, price, mark)
+VALUES(now(), 'Фуа-гра', 100500, TRUE);
+
+UPDATE products SET price = 80000 WHERE name like ('Фуа-гра');
+
+DELETE FROM products WHERE id BETWEEN 0 and 10;
