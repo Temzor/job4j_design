51d5e7a954d565525d46a313f44c925856501519
Капустин Дмитрий <onewaykd@gmail.com>
2021-12-22 23:49:11 +0300

    0.2. PrepareStatement [#379307 #242696]

--- src/main/java/ru/job4j/spammer/ImportDB.java
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -19,12 +19,14 @@ public class ImportDB {
         this.dump = dump;
     }
 
-    public List<User> load() throws IOException {
+    public List<User> load() {
         List<User> users = new ArrayList<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(dump))) {
             bufferedReader.lines()
                     .filter(l -> l.split(";").length > 1 && l.split(";")[1].contains("@"))
                     .forEach(l -> users.add(new User(l.split(";")[0], l.split(";")[1])));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return users;
     }
