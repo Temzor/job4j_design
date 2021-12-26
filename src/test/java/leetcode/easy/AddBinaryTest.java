26fc397d5e3fd2b5d51f8f1060d90080c2b72615
Капустин Дмитрий <onewaykd@gmail.com>
2021-12-26 12:13:38 +0300

    Leetcode: 67. Add Binary : Easy

---
+++ src/test/java/leetcode/easy/AddBinaryTest.java
@@ -0,0 +1,24 @@
+package leetcode.easy;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AddBinaryTest {
+    @Test
+    public void testOne() {
+        AddBinary addBinary = new AddBinary();
+        String a = "11";
+        String b = "1";
+        Assert.assertEquals("100", addBinary.addBinary(a, b));
+    }
+
+    @Test
+    public void testTwo() {
+        AddBinary addBinary = new AddBinary();
+        String a = "1010";
+        String b = "1011";
+        Assert.assertEquals("10101", addBinary.addBinary(a, b));
+    }
+
+}
