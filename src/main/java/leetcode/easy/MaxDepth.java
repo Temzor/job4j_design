3b4bd0b92e68d638b8c2f0c66471d98ebc0e6900
Капустин Дмитрий <onewaykd@gmail.com>
2022-01-25 15:57:26 +0300

    Leetcode: 104. Maximum Depth of Binary Tree : Easy

---
+++ src/main/java/leetcode/easy/MaxDepth.java
@@ -0,0 +1,11 @@
+package leetcode.easy;
+
+public class MaxDepth {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
+
