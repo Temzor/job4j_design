74698aa8fc303564cc47bd26b1d2d870c69c8b0c
Капустин Дмитрий <onewaykd@gmail.com>
2022-02-12 17:12:04 +0300

    Leetcode: 145. Binary Tree Postorder Traversal : Easy

---
+++ src/main/java/leetcode/easy/PostorderTraversal.java
@@ -0,0 +1,25 @@
+package leetcode.easy;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class PostorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode p = root;
+        while (!stack.isEmpty() || p != null) {
+            if (p != null) {
+                stack.push(p);
+                result.add(p.val);  // Add before going to children
+                p = p.left;
+            } else {
+                TreeNode node = stack.pop();
+                p = node.right;
+            }
+        }
+        return result;
+    }
+}
