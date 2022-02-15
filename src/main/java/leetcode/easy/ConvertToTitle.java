e6926d51c4ce6b6dd85de9fa57afe6fbbcf3d390
Капустин Дмитрий <onewaykd@gmail.com>
2022-02-15 15:59:20 +0300

    Leetcode: 168. Excel Sheet Column Title : Easy

---
+++ src/main/java/leetcode/easy/ConvertToTitle.java
@@ -0,0 +1,8 @@
+package leetcode.easy;
+
+public class ConvertToTitle {
+    public String convertToTitle(int columnNumber) {
+        return columnNumber == 0 ? "" : convertToTitle(--columnNumber / 26)
+                + (char) ('A' + (columnNumber % 26));
+    }
+}
---
+++ src/test/java/leetcode/easy/ConvertToTitleTest.java
@@ -0,0 +1,32 @@
+package leetcode.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ConvertToTitleTest {
+    @Test
+    public void testOne() {
+        int column = 1;
+        String expect = "A";
+        String convertToTitle = new ConvertToTitle().convertToTitle(column);
+        Assertions.assertEquals(expect, convertToTitle);
+    }
+
+    @Test
+    public void testTwo() {
+        int column = 28;
+        String expect = "AB";
+        String convertToTitle = new ConvertToTitle().convertToTitle(column);
+        Assertions.assertEquals(expect, convertToTitle);
+    }
+
+    @Test
+    public void tesThree() {
+        int column = 701;
+        String expect = "ZY";
+        String convertToTitle = new ConvertToTitle().convertToTitle(column);
+        Assertions.assertEquals(expect, convertToTitle);
+    }
+
+
+}
