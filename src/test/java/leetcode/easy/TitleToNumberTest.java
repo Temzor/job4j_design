27ff97264d89be48a40bc698dc3a41c81c80ca73
Капустин Дмитрий <onewaykd@gmail.com>
2022-02-16 11:37:20 +0300

    Leetcode: 171. Excel Sheet Column Number : Easy

---
+++ src/test/java/leetcode/easy/TitleToNumberTest.java
@@ -0,0 +1,32 @@
+package leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TitleToNumberTest {
+    @Test
+    public void testOne() {
+        TitleToNumber titleToNumber = new TitleToNumber();
+        String input = "A";
+        int output = 1;
+        Assert.assertEquals(output, titleToNumber.titleToNumber(input));
+    }
+
+    @Test
+    public void testTwo() {
+        TitleToNumber titleToNumber = new TitleToNumber();
+        String input = "AB";
+        int output = 28;
+        Assert.assertEquals(output, titleToNumber.titleToNumber(input));
+    }
+
+    @Test
+    public void testThree() {
+        TitleToNumber titleToNumber = new TitleToNumber();
+        String input = "ZY";
+        int output = 701;
+        Assert.assertEquals(output, titleToNumber.titleToNumber(input));
+    }
+
+
+}
