57dfba2387af537aa41c675cf61ba58df96ffb4e
Temzor <onewaykd@gmail.com>
2022-03-15 14:51:57 +0300

    CodeWars; Four/Seven; 7kyu.

---
+++ src/main/java/ru/codewars/eightkyu/FourSeven.java
@@ -0,0 +1,15 @@
+package ru.codewars.eightkyu;
+
+public class FourSeven {
+    public int fourSeven(int n) {
+        while (n == 4) {
+            return 7;
+        }
+
+        while (n == 7) {
+            return 4;
+        }
+
+        return 0;
+    }
+}
