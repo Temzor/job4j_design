24566b437d5b1dd23743617a75be735c6d5d8a72
Temzor <onewaykd@gmail.com>
2022-03-21 15:00:42 +0300

    0. Принцип единственной ответственности [#4913]

---
+++ src/test/java/ru/codewars/sevenkyu/FilterListTest.java
@@ -0,0 +1,18 @@
+package ru.codewars.sevenkyu;
+
+import org.junit.Test;
+import ru.codewars.eightkyu.FilterList;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class FilterListTest {
+
+    @Test
+    public void examples() {
+        assertEquals(Arrays.asList(1, 2), FilterList.filterList(Arrays.asList(1, 2, "a", "b")));
+        assertEquals(Arrays.asList(1, 0, 15), FilterList.filterList(Arrays.asList(1, "a", "b", 0, 15)));
+        assertEquals(Arrays.asList(1, 2, 123), FilterList.filterList(Arrays.asList(1, 2, "aasf", "1", "123", 123)));
+    }
+}
