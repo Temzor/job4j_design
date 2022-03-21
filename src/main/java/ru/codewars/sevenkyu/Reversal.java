533fda7f65e689cfcdb6ba6bd69398de0f308a70
Temzor <onewaykd@gmail.com>
2022-03-21 11:42:03 +0300

    CodeWars; Simple equation reversal; 7kyu.

--- src/main/java/ru/codewars/sevenkyu/Reversal.java
+++ src/main/java/ru/codewars/sevenkyu/Reversal.java
@@ -1,14 +1,14 @@
 package ru.codewars.sevenkyu;
 
-import java.util.regex.Pattern;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Reversal {
     public static String solve(String eq) {
-        var matcher = Pattern.compile("[a-z]|[+\\-*/]|\\d*").matcher(eq);
-        var answer = new StringBuilder();
-        while (matcher.find()) {
-            answer.insert(0, matcher.group());
-        }
-        return answer.toString();
+        List<String> list = Arrays.asList(eq.split("(?=\\b)"));
+        Collections.reverse(list);
+        return String.join("", list);
     }
 }
