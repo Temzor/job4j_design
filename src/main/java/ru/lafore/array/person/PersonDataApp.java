8a8c776c0228b7a3791fb0a6fe32233a90e886cb
Капустин Дмитрий <onewaykd@gmail.com>
2022-03-29 22:43:57 +0300

    Robert Lafore; Chapter 2; Arrays; 80 page.

---
+++ src/main/java/ru/lafore/array/person/PersonDataApp.java
@@ -0,0 +1,39 @@
+package ru.lafore.array.person;
+
+public class PersonDataApp {
+    public static void main(String[] args) {
+        int size = 100;
+        PersonDataArray personDataArray;
+        personDataArray = new PersonDataArray(size);
+
+        personDataArray.insert("Evans", "Patty", 24);
+        personDataArray.insert("Smith", "Lorraine", 37);
+        personDataArray.insert("Yee", "Tom", 43);
+        personDataArray.insert("Adams", "Henry", 63);
+        personDataArray.insert("Hashimoto", "Sato", 21);
+        personDataArray.insert("Stimson", "Henry", 29);
+        personDataArray.insert("Velasquez", "Jose", 72);
+        personDataArray.insert("Lamarque", "Henry", 54);
+        personDataArray.insert("Vang", "Minh", 22);
+        personDataArray.insert("Creswell", "Lucinda", 18);
+
+        personDataArray.displayApp();
+
+        String searchKey = "Stimson";
+        Person found;
+        found = personDataArray.find(searchKey);
+        if (found != null) {
+            System.out.print("Found ");
+            found.displayPerson();
+        } else {
+            System.out.println("Can't find " + searchKey);
+        }
+
+        System.out.println("Deleting Smith, Yee and Creswell");
+        personDataArray.delete("Smith");
+        personDataArray.delete("Yee");
+        personDataArray.delete("Creswell");
+
+        personDataArray.displayApp();
+    }
+}
---
+++ src/main/java/ru/lafore/array/person/PersonDataArray.java
@@ -0,0 +1,55 @@
+package ru.lafore.array.person;
+
+public class PersonDataArray {
+    private Person[] people;
+    private int nElements;
+
+    public PersonDataArray(int max) {
+        people = new Person[max];
+        nElements = 0;
+    }
+
+
+    public Person find(String searchName) {
+        int i;
+        for (i = 0; i < nElements; i++) {
+            if (people[i].getLastName().equals(searchName)) {
+                break;
+            }
+        }
+        if (i == nElements) {
+            return null;
+        } else {
+            return people[i];
+        }
+    }
+    
+    public void insert(String lastName, String firstName, int age) {
+        people[nElements] = new Person(lastName, firstName, age);
+        nElements++;
+    }
+    
+    public boolean delete(String searchName) {
+        int i;
+        for (i = 0; i < nElements; i++) {
+            if (people[i].getLastName().equals(searchName)) {
+                break;
+            }
+        }
+        if (i == nElements) {
+            return false;
+        } else {
+            for (int j = i; j < nElements; j++) {
+                people[j] = people[j + 1];
+            }
+            nElements--;
+            return true;
+        }
+    }
+
+    public void displayApp() {
+        for (int i = 0; i < nElements; i++) {
+            people[i].displayPerson();
+        }
+    }
+}
