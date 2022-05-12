0f1ccb302a7c378d76b0c2c02df4ed4da6d89190
Temzor <onewaykd@gmail.com>
2022-05-12 19:30:05 +0300

    Robert Lafore; Chapter 3; 119 page.

--- src/main/java/ru/lafore/chapter3/innersort/ArrayInnerSortApp.java
+++ src/main/java/ru/lafore/chapter3/innersort/ArrayInnerSortApp.java
@@ -4,23 +4,26 @@ import ru.lafore.chapter3.bublesort.ArrayBub;
 
 public class ArrayInnerSortApp {
     public static void main(String[] args) {
-        int maxSize = 100;
+        int maxSize = 100000;
         ArrayInnerSort arrayInnerSort;
         arrayInnerSort = new ArrayInnerSort(maxSize);
 
-        arrayInnerSort.insert(77);
-        arrayInnerSort.insert(99);
-        arrayInnerSort.insert(44);
-        arrayInnerSort.insert(55);
-        arrayInnerSort.insert(22);
-        arrayInnerSort.insert(88);
-        arrayInnerSort.insert(11);
-        arrayInnerSort.insert(0);
-        arrayInnerSort.insert(66);
-        arrayInnerSort.insert(33);
+        for (int i = 0; i < maxSize; i++) {
+            arrayInnerSort.insert((long) (Math.random() * (maxSize - 1)));
+        }
 
-        arrayInnerSort.display();
         arrayInnerSort.innerSort();
-        arrayInnerSort.display();
+
+        for (int i = 0; i < maxSize; i++) {
+            arrayInnerSort.insert(maxSize - i);
+        }
+
+        arrayInnerSort.innerSort();
+
+        for (int i = 0; i < maxSize; i++) {
+            arrayInnerSort.insert(i);
+        }
+
+        arrayInnerSort.innerSort();
     }
 }
--- src/test/java/ru/codewars/sevenkyu/PotatoesTest.java
+++ src/test/java/ru/codewars/sevenkyu/PotatoesTest.java
@@ -6,13 +6,13 @@ import static org.junit.Assert.assertEquals;
 
 public class PotatoesTest {
 
-    private static void doTest(int p0, int w0, int p1, int expected) {
+    private static void dotest(int p0, int w0, int p1, int expected) {
         assertEquals(expected, Potatoes.potatoes(p0, w0, p1));
     }
 
     @Test
     public void test() {
-        doTest(82, 127, 80, 114);
-        doTest(93, 129, 91, 100);
+        dotest(82, 127, 80, 114);
+        dotest(93, 129, 91, 100);
     }
 }
