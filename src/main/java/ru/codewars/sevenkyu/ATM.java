973708392a0c03d9a631e5c98abb29f951fb758a
Капустин Дмитрий <onewaykd@gmail.com>
2022-05-22 21:57:52 +0300

    CodeWars; ATM; 7kyu.

--- src/main/java/ru/codewars/sevenkyu/ATM.java
+++ src/main/java/ru/codewars/sevenkyu/ATM.java
@@ -1,32 +1,16 @@
 package ru.codewars.sevenkyu;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ATM {
     public int atm(int n) {
-        if (n % 10 > 1) {
-            return -1;
-        }
-        int counterNominal = 0;
-        int point = 0;
-        List<Integer> nominal = new ArrayList<>();
-        nominal.add(500);
-        nominal.add(200);
-        nominal.add(100);
-        nominal.add(50);
-        nominal.add(20);
-        nominal.add(10);
+        int[] denominations = new int[]{500, 200, 100, 50, 20, 10};
+        int result = 0;
 
-        while (n > 0) {
-            if (n % nominal.get(point) != n) {
-                n -= nominal.get(point);
-                counterNominal++;
-            } else {
-                point++;
-            }
+        for (int denomination : denominations) {
+            result += n / denomination;
+            n %= denomination;
         }
-        return counterNominal;
+
+        return n == 0 ? result : -1;
     }
 }
