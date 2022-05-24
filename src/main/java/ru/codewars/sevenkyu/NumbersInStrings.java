63c84b65a021d5643591113715a21682cda2e44e
Капустин Дмитрий <onewaykd@gmail.com>
2022-05-24 22:53:35 +0300

    CodeWars; Numbers in strings; 7kyu.

--- src/main/java/ru/codewars/sevenkyu/NumbersInStrings.java
+++ src/main/java/ru/codewars/sevenkyu/NumbersInStrings.java
@@ -1,9 +1,9 @@
 package ru.codewars.sevenkyu;
 
-import static java.util.stream.Stream.of;
+import java.util.Arrays;
 
 public class NumbersInStrings {
     public static int numbersInStrings(String s) {
-        return of(s.split("\\D")).mapToInt(n -> n.isEmpty() ? 0 : Integer.parseInt(n)).max().orElse(0);
+        return Arrays.stream(s.split("[a-z]+")).filter(e -> !e.isEmpty()).mapToInt(Integer::parseInt).max().getAsInt();
     }
 }
