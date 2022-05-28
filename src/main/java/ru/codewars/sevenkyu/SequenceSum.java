e36dc3295bad0a8c6ad5bd0784f5a7ee5f161c8c
Капустин Дмитрий <onewaykd@gmail.com>
2022-05-28 21:26:33 +0300

    CodeWars; Basic Sequence Practice; 7kyu.

--- src/main/java/ru/codewars/sevenkyu/SequenceSum.java
+++ src/main/java/ru/codewars/sevenkyu/SequenceSum.java
@@ -2,18 +2,10 @@ package ru.codewars.sevenkyu;
 
 public class SequenceSum {
     public static int[] sumOfN(int n) {
-
-        int[] sum = new int[Math.abs(n) + 1];
-
-        if (n < 0) {
-            for (int i = -1; i >= n; i--) {
-                sum[Math.abs(i)] = i + sum[Math.abs(i) - 1];
-            }
+        int[] result = new int[Math.abs(n) + 1];
+        for (int i = 1; i < result.length; i++) {
+            result[i] = result[i - 1] + (n > 0 ? i : -i);
         }
-        for (int i = 1; i <= n; i++) {
-            sum[i] = i + sum[i - 1];
-        }
-
-        return sum;
+        return result;
     }
 }
